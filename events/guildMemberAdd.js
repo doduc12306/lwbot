e89e7d21332c6704152ad7f64efdb0bffae4fd92
jennasisis <jennasisis@gmail.com>
2018-06-04 22:10:07 -0400

    added *not optional* dbans system

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,6 @@
-// This event executes when a new member joins a server. Let's welcome them!
+// This event executes when a new member joins a server. Let's welcome them! (or ban them!)
+var Dbans = require(`dbans`);
+const Discord = require(`discord.js`);
 
 module.exports = (client, member) => {
   // Load the guild's settings
@@ -13,4 +15,26 @@ module.exports = (client, member) => {
   // Send the welcome message to the welcome channel
   // There's a place for more configs here.
   member.guild.channels.find(`name`, settings.welcomeChannel).send(welcomeMessage).catch(console.error);
+
+  Dbans = new Dbans(client.config.dbans_token);
+  Dbans.check(member.user.id)
+    .then(async user => {
+      if (!user) return;
+
+      var banEmbed = new Discord.RichEmbed()
+        .setColor(`0xFF0000`)
+        .setFooter(`Courtesy of bans.discordlist.net | Ban ID: ${user.banID}`)
+        .setAuthor(member.user.tag, member.user.avatarURL)
+        .setThumbnail(`https://cdn.discordapp.com/avatars/435539650149482526/1893e7033db3896e2f530b8023488fc3.png`)
+        .setTitle(`\`:x: Banned from ${member.guild.name}\``)
+        .addField(`Reason:`, user.reason)
+        .addField(`Proof:`, user.proof);
+
+      await user.send(banEmbed);
+      await client.wait(1000);
+      await member.guild.ban(member.user, {reason: `Auto ban: check the mod logs`});
+      await client.wait(2000);
+      await member.guild.channels.find(`name`, settings.modLogChannel).send(banEmbed);
+    })
+    .catch(err => client.users.get(client.config.ownerID).send(err));
 };
