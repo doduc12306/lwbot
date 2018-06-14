49fbd2fec48b583bb860e073f6bf864b18b6d065
Akihito Mikazuki <jennasisis@gmail.com>
2018-06-14 13:33:15 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ clear;
 npm i chalk dbans discord.js enmap enmap-level giphy-api moment pm2 sequelize sqlite3 urban-dictionary utf8;
 echo " ";
 echo " ";
-echo "-------------- STARTING PM2 DAEMON --------------";
+echo "-------------- STARTING -------------";
 echo " ";
 echo " ";
-pm2 start index.js --attach;
+node index.js
