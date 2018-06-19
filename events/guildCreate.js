88e9f6694103bb2c9827dada9ea5be0d5ff925b6
jennasisis <jennasisis@gmail.com>
2018-06-19 12:12:06 -0400

    guild.id check (see desc.) This was a PSA from York.Dev, basically saying that this guild is full of usersbots spamming commands. if the bot joins this guild, it'll see extreme performance issues, as stated in the comments

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,10 @@
 // This event executes when a new guild (server) is joined.
 
 module.exports = (client, guild) => {
+
+  // Thanks for this PSA, York.
+  // This guild is full of userbots spamming commands. If this bot enters it, you'll see extreme performance issues.
+  if(guild.id === `439438441764356097`) return guild.leave();
   // We need to add this guild to our settings!
   client.settings.set(guild.id, client.config.defaultSettings);
 };
