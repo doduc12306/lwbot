6735baafeaea86b458e5403f54941356b3cc2d6c
jennasisis <jennasisis@gmail.com>
2018-06-20 14:47:07 -0400

    added sequelize to require list for easier access

--- commands/eval.js
+++ commands/eval.js
@@ -9,6 +9,7 @@
 const { inspect } = require(`util`);
 const { post } = require(`snekfetch`);
 const Discord = require(`discord.js`);
+const Sequelize = require(`sequelize`);
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   const code = args.join(` `);
