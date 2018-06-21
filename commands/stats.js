e97d40c9391f129bb1606ba863d40a1cb1bad8c5
jennasisis <jennasisis@gmail.com>
2018-06-21 14:59:35 -0400

    uptime is prettier

--- commands/stats.js
+++ commands/stats.js
@@ -1,19 +1,13 @@
 const { version } = require(`discord.js`);
 const moment = require(`moment`);
 const Discord = require(`discord.js`);
+require(`moment-duration-format`);
 
 exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
-
-  var hours = (Math.round(client.uptime / (1000 * 60 * 60)));
-  var days = (Math.floor(hours / 24));
-  var finHours = (hours - days * 24);
-  var minutes = (Math.round(client.uptime / (1000 * 60)) % 60);
-  var seconds = (Math.round(client.uptime / 1000) % 60);
-
   message.channel.send(new Discord.RichEmbed()
     .setTitle(`\`Statistics\``)
     .addField(`Mem Usage`, `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`)
-    .addField(`Uptime`, `${days} D, ${finHours} H, ${minutes} M, ${seconds} S`)
+    .addField(`Uptime`, moment.duration(client.uptime).format(`M [months] W [weeks] D [days], H [hrs], m [mins], s [secs]`))
     .addField(`Discord.js`, `v${version}`)
     .addField(`Node`, process.version)
     .setColor(`0x59D851`)
