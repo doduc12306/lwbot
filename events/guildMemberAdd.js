b33b3c65a71638d45e84e888f4301c9219d61ca1
jennasisis <jennasisis@gmail.com>
2018-06-21 09:36:23 -0400

    removed dbans

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,32 +1,9 @@
-// This event executes when a new member joins a server. Let's welcome them! (or ban them!)
-var Dbans = require(`dbans`);
+// This event executes when a new member joins a server. Let's welcome them!
 const Discord = require(`discord.js`);
 
 module.exports = (client, member) => {
   // Load the guild's settings
   const settings = client.settings.get(member.guild.id);
-
-  Dbans = new Dbans(client.config.dbans_token);
-  Dbans.check(member.user.id)
-    .then(async user => {
-      if (user === false) return;
-
-      var banEmbed = new Discord.RichEmbed()
-        .setColor(`0xFF0000`)
-        .setFooter(`Courtesy of bans.discordlist.net | Ban ID: ${user.banID}`)
-        .setAuthor(member.user.tag, member.user.avatarURL)
-        .setThumbnail(`https://cdn.discordapp.com/avatars/435539650149482526/1893e7033db3896e2f530b8023488fc3.png`)
-        .setTitle(`\`:x: Banned from ${member.guild.name}\``)
-        .addField(`Reason:`, user.reason)
-        .addField(`Proof:`, user.proof);
-
-      await user.send(banEmbed);
-      await client.wait(1000);
-      await member.guild.ban(member.user, {reason: `Auto ban: check the mod logs`});
-      await client.wait(2000);
-      await member.guild.channels.find(`name`, settings.modLogChannel).send(banEmbed);
-    })
-    .catch(err => client.users.get(client.config.ownerID).send(`DBans error: ${err}`));
 
   // If welcome is off, don't proceed (don't welcome the user)
   if (settings.welcomeEnabled !== `true`) return;
