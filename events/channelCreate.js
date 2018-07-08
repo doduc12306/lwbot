d9a674e5002d52402311a8d89645317343e8c2f4
Samir Buch <akii@MacBook.fios-router.home>
2018-07-07 20:36:20 -0400

    automatically adds muted perms to a channel

---
+++ events/channelCreate.js
@@ -0,0 +1,16 @@
+module.exports = (client, channel) => {
+    if(!channel.guild.me.permissions.has('MANAGE_ROLES')) return channel.guild.owner.send(`:x: **Error editing \`${channel.name}\`'s perms:**\nMissing Permission: \`Manage Roles\`.`);
+
+    var role = channel.guild.roles.find('name', 'Muted') || channel.guild.roles.find('name', 'muted');
+
+    if(!role) {
+        role = channel.guild.createRole({name: 'Muted'});
+        channel.guild.channels.forEach(g => {
+            if(g.type === 'text') g.overwritePermissions(role.id, {SEND_MESSAGES: false, ADD_REACTIONS: false});
+            if(g.type === 'voice') g.overwritePermissions(role.id, {CONNECT: false, SPEAK: false});
+        });
+    }
+
+    if(channel.type === 'text') return channel.overwritePermissions(role.id, {SEND_MESSAGES: false, ADD_REACTIONS: false});
+    if(channel.type === 'voice') return channel.overwritePermissions(role.id, {CONNECT: false, SPEAK: false});
+};
