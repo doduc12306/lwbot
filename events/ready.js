c1ce9c783cf6cf3b8392fb1aebb2348de2c29ffd
akii0008 <akii@Samirs-MacBook.local>
2018-07-10 15:43:09 -0400

    role menu now has persistance

--- events/ready.js
+++ events/ready.js
@@ -123,4 +123,156 @@ module.exports = async client => {
   setInterval(() => {
     client.channels.get(`447132033173422090`).bulkDelete(1);
   }, 8.64e+7);
+
+  // Support for the role menus for Webtoon Central
+
+  var genre = client.channels.get('444375693728546816');
+  genre.fetchMessage('466280999760953355')
+  .then(async msg => {
+    const collector = msg.createReactionCollector((reaction, user) => 
+      reaction.emoji.name === "⚔" ||
+      reaction.emoji.name === "💪" ||
+      reaction.emoji.name === "❤" ||
+      reaction.emoji.name === "👻" || 
+      reaction.emoji.name === "🏀" || 
+      reaction.emoji.name === "📔" ||
+      reaction.emoji.name === "🤣"
+    )
+    collector.on("collect", reaction => {
+      const chosen = reaction.emoji.name;
+      var member = reaction.users.last().id
+      member = genre.guild.members.get(member);
+
+      function toggleRole(role) {
+        if(!member.roles.has(role)) member.addRole(role);
+        else member.removeRole(role);
+      }
+
+      if(chosen === "⚔") {
+        toggleRole('444346550760636417');
+      } else if(chosen === "💪") {
+        toggleRole('444396478446829568');
+      } else if(chosen === "❤") {
+        toggleRole('444346546142838784');
+      } else if(chosen === "👻") {
+        toggleRole('444346749390159872');
+      } else if(chosen === "🏀"){
+        toggleRole('444346752976551936');
+      } else if(chosen === "📔") {
+        toggleRole('444346756159766536');
+      } else if(chosen === "🤣") {
+        toggleRole('444347123769802754');
+      }
+    }); 
+  })
+
+  var ping = client.channels.get('440974386544115713')
+  ping.fetchMessage('466281139418693633')
+  .then(async msg => {
+    const collector = msg.createReactionCollector((reaction, user) => 
+    reaction.emoji.name === "📌" ||
+    reaction.emoji.name === "🍿" ||
+    reaction.emoji.name === "🕹" ||
+    reaction.emoji.name === "🎤" || 
+    reaction.emoji.name === "😍" || 
+    reaction.emoji.name === "🤝" ||
+    reaction.emoji.name === "✍" ||
+    reaction.emoji.name === "🎨" || 
+    reaction.emoji.name === "💜" || 
+    reaction.emoji.name === "📝"
+  )
+  collector.on("collect", reaction => {
+    const chosen = reaction.emoji.name;
+    var member = reaction.users.last().id
+    member = ping.guild.members.get(member);
+
+    function toggleRole(role) {
+      if(!member.roles.has(role)) member.addRole(role);
+      else member.removeRole(role);
+    }
+
+    if(chosen === "📌") {
+      toggleRole('432633011515949067');
+    } else if(chosen === "🍿") {
+      toggleRole('440974703062941696');
+    } else if(chosen === "🕹") {
+      toggleRole('440974647975215125');
+    } else if(chosen === "🎤") {
+      toggleRole('455182908551069697');
+    } else if(chosen === "😍"){
+      toggleRole('458434931899498518');
+    } else if(chosen === "🤝") {
+      toggleRole('440974803940278293');
+    } else if(chosen === "✍") {
+      toggleRole('458436541694607361');
+    } else if(chosen === "🎨"){
+      toggleRole('458436569662226442');
+    } else if(chosen === "💜"){
+      toggleRole('442896867307683842');
+    } else if(chosen === "📝"){
+      toggleRole('453294003002015744');
+    }
+  }); 
+});
+
+var misc = client.channels.get('444375656139063296');
+misc.fetchMessage('466281176420843524')
+.then(async msg => {
+  const collector = msg.createReactionCollector((reaction, user) => 
+  reaction.emoji.name === "📩" ||
+  reaction.emoji.name === "⛔️" ||
+  reaction.emoji.name === "❓" ||
+  reaction.emoji.name === "💻" || 
+  reaction.emoji.name === "🎮" || 
+  reaction.emoji.name === "💚" ||
+  reaction.emoji.name === "✌️" ||
+  reaction.emoji.name === "🎥" || 
+  reaction.emoji.name === "👁" || 
+  reaction.emoji.name === "📓" || 
+  reaction.emoji.name === "📘" || 
+  reaction.emoji.name === "📕" || 
+  reaction.emoji.name === "📗" || 
+  reaction.emoji.name === "📙"
+)
+collector.on("collect", reaction => {
+  const chosen = reaction.emoji.name;
+  var member = reaction.users.last().id
+  member = message.guild.members.get(member);
+
+  function toggleRole(role) {
+    if(!member.roles.has(role)) member.addRole(role);
+    else member.removeRole(role);
+  }
+
+  if(chosen === "📩") {
+    toggleRole('444347837560520704');
+  } else if(chosen === "⛔️") {
+    toggleRole('444347835060846612');
+  } else if(chosen === "❓") {
+    toggleRole('444347831864524800');
+  } else if(chosen === "💻") {
+    toggleRole('444347838235672596');
+  } else if(chosen === "🎮"){
+    toggleRole('444347839091572736');
+  } else if(chosen === "💚") {
+    toggleRole('444348193568718849');
+  } else if(chosen === "✌️") {
+    toggleRole('444348188975955969');
+  } else if(chosen === "🎥"){
+    toggleRole('444348190771380226');
+  } else if(chosen === "👁"){
+    toggleRole('444390328158650388');
+  } else if(chosen === "📓"){
+    toggleRole('444340936286273538');
+  } else if(chosen === "📘"){
+    toggleRole('444291891899662346');
+  } else if(chosen === "📕"){
+    toggleRole('444291781031493633');
+  } else if(chosen === "📗"){
+    toggleRole('444346408670330890');
+  } else if(chosen === "📙"){
+    toggleRole('444340936286273538');
+  }
+}); 
+});
 };
