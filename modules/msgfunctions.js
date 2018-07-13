d02a2f8c357379acc27c96f96eda27e9edb44f6b
akii0008 <akii@Samirs-MacBook.local>
2018-07-12 22:30:13 -0400

    added a demographic

--- modules/msgfunctions.js
+++ modules/msgfunctions.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-module.exports = (message) => {
+module.exports = async (message) => {
   var xpTable = new Sequelize('database', 'user', 'password', {
     host: 'localhost',
     dialect: 'sqlite',
@@ -24,8 +24,8 @@ module.exports = (message) => {
     message.xp.findOne({where: {user: userID}}).then(async user => {
       if(user === null) {
         await message.xp.create({user: userID, xp: 0});
-        await message.xp.update({xp: user.xp + amount}, {where: {user: userID}});
-        message.xp.sync();
+        await message.xp.sync();
+        message.xp.update({xp: user.xp + amount}, {where: {user: userID}});
       }
       else {
         await message.xp.update({xp: user.xp + amount}, {where: {user: userID}});
@@ -72,4 +72,57 @@ module.exports = (message) => {
       });
     });
   };
+
+  // Demographic portion!w eval 
+
+  var demoTable = new Sequelize('database', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'databases/wcDemographic.sqlite'
+  });
+
+  message.demo = demoTable.define('info', {
+    time: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    messages: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    }
+  }, {timestamps: false});
+  demoTable.sync();
+
+  message.demo.add = () => {
+    var date = new Date();
+    date = date.toString();
+    date = date.substring(0, 21)
+    message.demo.findOne({where: {time: date}}).then(async data => {
+      if(data === null) {
+        await message.demo.create({time: date, messages: 1});
+        await message.demo.sync();
+        await message.demo.update({messages: data.messages + 1}, {where: {time: date}});
+      }
+      else {
+        await message.demo.update({messages: data.messages + 1}, {where: {time: date}});
+        await message.demo.sync();
+      }
+    });
+  }
+
+  setInterval(() => {
+    var date = new Date();
+    date = date.toString();
+    time = date.substring(22, 24);
+    date = date.substring(0, 21)
+    console.log(time);
+    message.demo.findOne({where: {time: date}}).then(async data => {
+      if(data === null && time === "59") {
+        await message.demo.create({time: date, messages: 0});
+        await message.demo.sync();
+      }
+    })
+  }, 500);
 };
