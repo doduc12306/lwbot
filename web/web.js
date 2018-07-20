73af1b5ccbb5e85a6702d1fe957f4cdfdcb4bbeb
akii0008 <akii@Samirs-MacBook.local>
2018-07-20 16:32:37 -0400

    added auth functionaliy

--- web/web.js
+++ web/web.js
@@ -1,20 +1,49 @@
 const express = require('express');
 var app = express();
 const path = require('path');
+const config = require('../config.js');
+var snek = require('snekfetch');
 
 app.use(express.static('assets'));
 
-express.addPage = (page) => {
-  app.get(`/${page}`, (req, res) => res.sendFile(path.join(__dirname, `./${page}.html`)));
-}
+app.listen(8080, () => console.log('Website listening on port 8080!'));
 
-app.get(`/`, (req, res) => res.sendFile(path.join(__dirname, `./index.html`)));
+express.addPage = (page) => app.get(`/${page}`, (req, res) => res.sendFile(path.join(__dirname, `./${page}.html`)));
+
+app.get(`/`, async (req, res) => res.sendFile(path.join(__dirname, `./index.html`)));
+
+app.get('/authorizing', async (req, res) => {
+  var data = {
+    'client_id': '377205339323367425',
+    'client_secret': config.client_secret,
+    'grant_type': 'authorization_code',
+    'code': req.query.code,
+    'redirect_uri': 'http://localhost:8080/authorizing/'
+  }
+
+  if(req.query.code === undefined) return;
+  try {
+    await snek.post(`https://discordapp.com/api/v6/oauth2/token`, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}, data: data});
+    res.sendFile(path.join(__dirname, './error-pages/auth-success.html'));
+    //setTimeout(() => res.sendFile(path.join(__dirname, `./index.html`)), 3000);
+  } catch (e) {
+    res.sendFile(path.join(__dirname, './error-pages/auth-error.html'));
+    //setTimeout(() => res.sendFile(path.join(__dirname, `./index.html`)), 3000);
+  }
+});
+
+app.get('/login', (req, res) => {
+  res.redirect('https://discordapp.com/api/oauth2/authorize?client_id=377205339323367425&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauthorizing%2F&response_type=code&scope=identify%20guilds');
+})
 
 express.addPage('commands');
-express.addPage('login');
-
-app.listen(8080, () => console.log('Website listening on port 8080!'))
+express.addPage('authorizing');
 
 app.use(function (req, res, next) {
-  res.status(404).sendFile(path.join(__dirname, './404.html'));
+  res.status(404).sendFile(path.join(__dirname, './error-pages/404.html'));
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).sendFile(path.join(__dirname, './error-pages/500.html'));
 });
