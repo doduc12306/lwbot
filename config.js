1ed159e982a4717dd8358f7e677d7bc6ec718f95
Akihito Mikazuki <jennasisis@gmail.com>
2018-07-31 22:29:09 -0400

    config: added client_secret

--- config.js
+++ config.js
@@ -1,23 +1,25 @@
 const config = {
   // Bot Owner, level 10 by default. A User ID. Should never be anything else than the bot owner's ID.
   "ownerID": `107599228900999168`,
-  
+
   // Bot Admins, level 9 by default. Array of user ID strings.
   "admins": [],
-  
+
   // Bot Support, level 8 by default. Array of user ID strings
   "support": [],
-  
+
   // Your Bot's Token. Available on https://discordapp.com/developers/applications/me
   "token": `Mzc3MjA1MzM5MzIzMzY3NDI1.Dar1YA.0AIexER1daemthd1bG84DJJPHKQ`,
   "giphy": `y118kMRynO3XKfcHrRkpuWQ9CEXrqDfg`,
   "debugtoken": `Mzk0OTEzOTAzNDY2OTA1NjAx.DfXfBA.WbzI-ijiZYKxQJAftJ8S029RjE4`,
-  
-  // Default per-server settings. New guilds have these settings. 
-  
+
+  "client_secret": 'TWqz7cHZY5mrIaHpB1FFFVtSWXFocpce',
+
+  // Default per-server settings. New guilds have these settings.
+
   // DO NOT LEAVE ANY OF THESE BLANK, AS YOU WILL NOT BE ABLE TO UPDATE THEM
   // VIA COMMANDS IN THE GUILD.
-    
+
   "defaultSettings" : {
     "prefix": `!w `,
     "modLogChannel": `mod_logs`,
@@ -46,11 +48,11 @@ const config = {
   },
 
   // PERMISSION LEVEL DEFINITIONS.
-  
+
   permLevels: [
     // This is the lowest permisison level, this is for non-roled users.
     { level: 0,
-      name: `User`, 
+      name: `User`,
       // Don't bother checking, just return true which allows them to execute any command their
       // level allows them to.
       check: () => true
@@ -73,9 +75,9 @@ const config = {
         }
       }
     },
-  
+
     { level: 3,
-      name: `Administrator`, 
+      name: `Administrator`,
       check: (message) => {
         try {
           const adminRole = message.guild.roles.find(r => r.name.toLowerCase() === message.settings.adminRole.toLowerCase());
@@ -98,12 +100,12 @@ const config = {
     },
     // This is the server owner.
     { level: 5,
-      name: `Server Owner`, 
+      name: `Server Owner`,
       // Simple check, if the guild owner id matches the message author's ID, then it will return true.
       // Otherwise it will return false.
       check: (message) => message.channel.type === `text` ? (message.guild.owner.user.id === message.author.id ? true : false) : false
     },
-  
+
     // Bot Support is a special inbetween level that has the equivalent of server owner access
     // to any server they joins, in order to help troubleshoot the bot on behalf of owners.
     { level: 8,
@@ -112,22 +114,22 @@ const config = {
       // change this and reboot the bot to add a support user. Make it better yourself!
       check: (message) => config.support.includes(message.author.id)
     },
-  
+
     // Bot Admin has some limited access like rebooting the bot or reloading commands.
     { level: 9,
       name: `Bot Admin`,
       check: (message) => config.admins.includes(message.author.id)
     },
-  
+
     // This is the bot owner, this should be the highest permission level available.
     // The reason this should be the highest level is because of dangerous commands such as eval
     // or exec (if the owner has that).
     { level: 10,
-      name: `Bot Owner`, 
+      name: `Bot Owner`,
       // Another simple check, compares the message author id to the one stored in the config file.
       check: (message) => message.client.config.ownerID === message.author.id
     }
   ]
 };
-  
+
 module.exports = config;
