f01957f44bf69144c7f5aa80aeb6a3a8baa9983d
Akihito Mikazuki <jennasisis@gmail.com>
2018-07-31 22:30:12 -0400

    web: web.js: added terms controller also moved the code query to above data var

--- web/web.js
+++ web/web.js
@@ -13,6 +13,8 @@ express.addPage = (page) => app.get(`/${page}`, (req, res) => res.sendFile(path.
 app.get(`/`, async (req, res) => res.sendFile(path.join(__dirname, `./index.html`)));
 
 app.get('/authorizing', async (req, res) => {
+  if (req.query.code === undefined) return;
+
   var data = {
     'client_id': '377205339323367425',
     'client_secret': config.client_secret,
@@ -21,24 +23,22 @@ app.get('/authorizing', async (req, res) => {
     'redirect_uri': 'http://localhost:8080/authorizing/'
   }
 
-  if(req.query.code === undefined) return;
   try {
     await snek.post(`https://discordapp.com/api/v6/oauth2/token`, {headers: {'Content-Type': 'application/x-www-form-urlencoded'}, data: data});
     res.sendFile(path.join(__dirname, './error-pages/auth-success.html'));
-    //setTimeout(() => res.sendFile(path.join(__dirname, `./index.html`)), 3000);
   } catch (e) {
     res.sendFile(path.join(__dirname, './error-pages/auth-error.html'));
-    //setTimeout(() => res.sendFile(path.join(__dirname, `./index.html`)), 3000);
   }
 });
 
 app.get('/login', (req, res) => {
   res.redirect('https://discordapp.com/api/oauth2/authorize?client_id=377205339323367425&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauthorizing%2F&response_type=code&scope=identify%20guilds');
-})
+});
 
 express.addPage('commands');
 express.addPage('authorizing');
 express.addPage('stats');
+express.addPage('terms');
 
 app.use(function (req, res, next) {
   res.status(404).sendFile(path.join(__dirname, './error-pages/404.html'));
