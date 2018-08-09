e055413014ea0b6103b1a86b421f7e609e10e332
Akihito Mikazuki <jennasisis@gmail.com>
2018-08-08 22:58:23 -0400

    msgfunctions: message.guild.settings is complete. Now onto the rest of the bot that requires enmap to do the work Oh and i disabled timestamps for the table

--- modules/msgfunctions.js
+++ modules/msgfunctions.js
@@ -121,8 +121,66 @@ module.exports = async (message) => {
      allowNull: false
    },
    value: Sequelize.STRING
-  });
+  }, {timestamps: false});
   guildTable.sync();
 
+  message.guild.settings.add = (key, value) => {
+    return new Promise((resolve, reject) => {
+      if(!key) return reject(new Error('Missing key to add'));
+      if(!value) return reject(new Error('Missing value to add'));
+      if(typeof key !== 'string') return reject(new Error(`"${key}" is not a string`));
+      if(typeof value !== 'string') return reject(new Error(`"${value}" is not a string`));
+      if(key.includes(' ')) return reject(new Error(`"${key}" must be one word`));
+
+      message.guild.settings.create({key: key, value: value})
+        .then(() => {
+          return resolve(true);
+        })
+        .catch(e => {
+          return reject(new Error(e));
+        });
+    });
+  }
+
+  message.guild.settings.delete = key => {
+    return new Promise((resolve, reject) => {
+      if(!key) return reject(new Error('Missing key to delete'));
+      if(typeof key !== 'string') return reject(new Error(`"${key}" is not a string`));
+      message.guild.settings.findOne({where: {key: key}}).then(data => {
+        if(data === null) return reject(new Error(`The key "${key}" does not exist to delete`));
+        message.guild.settings.destroy({where: {key: key}});
+        return resolve(true);
+      });
+    });
+  }
+  message.guild.settings.remove = key => message.guild.settings.delete(key);
+
+  message.guild.settings.edit = (key, newValue) => {
+    return new Promise((resolve, reject) => {
+      if(!key) return reject(new Error('Missing key to edit'));
+      if(!newValue) return reject(new Error('Missing value to edit'));
+      if(typeof key !== 'string') return reject(new Error(`"${key}" is not a string`));
+      if(typeof newValue !== 'string') return reject(new Error(`"${newValue}" is not a string`));
+
+      message.guild.settings.findOne({where: {key: key}}).then(data => {
+        if(data === null) return reject(new Error(`The key "${key}" does not exist to edit`));
+        message.guild.settings.update({value: newValue}, {where: {key: key}});
+        return resolve(true);
+      });
+    });
+  }
+  message.guild.settings.set = (key, newValue) => message.guild.settings.edit(key, newValue);
+
+  message.guild.settings.get = key => {
+    return new Promise((resolve, reject) => {
+      if(!key) return reject(new Error('Missing key to get'));
+      if(typeof key !== 'string') return reject(new Error(`"${key}" is not a string`));
+
+      message.guild.settings.findOne({where: {key: key}}).then(data => {
+        if(data === null) return reject(new Error(`"${key}" does not exist to get`));
+        return resolve(data.value);
+      });
+    });
+  }
 
 };
