f015b10086c5b78b6080a40a975aae3692a42b55
Akihito Mikazuki <jennasisis@gmail.com>
2018-08-09 13:49:10 -0400

    ratelimit event: D.js 11.4 added this event, so i used it lol

---
+++ events/ratelimit.js
@@ -0,0 +1,6 @@
+module.exports = (client, rateLimitInfo) => {
+  client.logger.error(`Ratelimited!`);
+  console.error(rateLimitInfo);
+  client.logger.error('Process will shut down now.');
+  process.exit(1);
+};
