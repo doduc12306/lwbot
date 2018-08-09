6268dbf0aaebda586acd1e765c184150401ecf0a
Akihito Mikazuki <jennasisis@gmail.com>
2018-08-08 23:43:35 -0400

    guildCreate: added default values to settings

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,20 +1,22 @@
 const Discord = require(`discord.js`);
 const Sequelize = require(`sequelize`);
 
-module.exports = (client, guild) => {
+module.exports = async (client, guild) => {
 
   // Thanks for this PSA, York. This guild is full of userbots spamming commands. If this bot enters it, you'll see extreme performance issues.
   if(guild.id === `439438441764356097`) return guild.leave();
- 
+
   // We need to add this guild to our settings!
   if(!client.settings.get(guild.id)) client.settings.set(guild.id, client.config.defaultSettings);
 
-  var modBase = new Sequelize(`database`, `user`, `password`, {
+  var guildTable = new Sequelize(`database`, `user`, `password`, {
     host: `localhost`,
     dialect: `sqlite`,
     storage: `databases/servers/${guild.id}.sqlite`
   });
-  modBase = modBase.define(`moderation`, {
+
+  // Modbase initialization
+  modBase = guildTable.define(`moderation`, {
     victim: {
       type: Sequelize.STRING,
       allowNull: false
@@ -30,7 +32,28 @@ module.exports = (client, guild) => {
     reason: Sequelize.STRING,
     duration: Sequelize.STRING
   });
-  modBase.sync();
+  guildTable.sync();
+
+  // Guild settings intialization
+  guild.settings = await guildTable.define('settings', {
+    key: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    value: Sequelize.STRING
+  }, {timestamps: false});
+  await guildTable.sync();
+
+  guild.settings.create({key: 'prefix', value: '!w '});
+  guild.settings.create({key: 'modLogChannel', value: 'mod_logs'});
+  guild.settings.create({key: 'modRole', value: 'Mods'});
+  guild.settings.create({key: 'adminRole', value: 'Admins'});
+  guild.settings.create({key: 'systemNotice', value: 'true'});
+  guild.settings.create({key: 'welcomeEnabled', value: 'false'});
+  guild.settings.create({key: 'welcomeChannel', value: 'welcome'});
+  guild.settings.create({key: 'welcomeMessage', value: 'Welcome to the server, {{user}}!'});
+  guild.settings.create({key: 'announcementChannel', value: 'announcements'});
+  guildTable.sync();
 
   if(!guild.me.permissions.has('SEND_MESSAGES')) guild.owner.send(':x: **CRITICAL PERMISSION MISSING:** \`Send Messages\` **WHICH EVERYTHING REQUIRES!**');
 
