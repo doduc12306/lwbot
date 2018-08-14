f4179e36a22af358c5da332a40fd52628016a453
Akihito Mikazuki <jennasisis@gmail.com>
2018-08-14 13:44:03 -0400

    info: updated info to match right now

--- commands/info.js
+++ commands/info.js
@@ -1,15 +1,14 @@
 const Discord = require(`discord.js`);
 
-
 module.exports.run = (client, message, args) => { // eslint-disable-line no-unused-vars
   message.channel.send(new Discord.RichEmbed()
     .setColor(`0x59D851`)
-    .setAuthor(`LINE WEBTOON Bot`, client.user.avatarURL)
+    .setAuthor(`LINE WEBTOON`, client.user.avatarURL)
     .setThumbnail(client.user.avatarURL)
     .addField(`Version`, require(`../package.json`).version, true)
     .addField(`Library`, `[Discord.js](http://discord.js.org/)`, true)
     .addField(`Creators`, `<@107599228900999168>`, true)
-    .addField(`Honorable Mentions`, `[\`An Idiot's Guide\`](http://anidiots.guide/) - Command handler, eval command, reload command, and so many other things. Thank you *so* much.\n[\`discordjs.guide\`](http://discordjs.guide) - Provided currency system and tagging system. You guys are amazing.\n[\`Discord.js Server\`](https://discord.gg/bRCvFy9) - You guys answered every question the docs couldn't. Without you, this bot would not be possible.`)
+    .addField(`Honorable Mentions`, `[\`An Idiot's Guide\`](http://anidiots.guide/) - Command handler, eval command, reload command, and so many other things. Thank you *so* much.\n[\`discordjs.guide\`](http://discordjs.guide) - Provided tagging system.\n[\`Discord.js Server\`](https://discord.gg/bRCvFy9) - You guys answered every question the docs couldn't.`)
   );
 };
 
