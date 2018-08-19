c7151b4a967b8b348a9ecca72e63de05094202be
Akihito Mikazuki <jennasisis@gmail.com>
2018-08-19 18:44:36 -0400

    set: reworked set to work with the databases

--- commands/set.js
+++ commands/set.js
@@ -1,4 +1,5 @@
 const { inspect } = require(`util`);
+const Discord = require('discord.js');
 
 // Note the **destructuring** here. instead of `args` we have :
 // [action, key, ...value]
@@ -7,72 +8,86 @@ const { inspect } = require(`util`);
 // OR the same as:
 // const [action, key, ...value] = args;
 exports.run = async (client, message, [action, key, ...value], level) => { // eslint-disable-line no-unused-vars
+  message.guild.settings.findAll().then(data => {
+    if(action === 'view') {
+      if(key) {
+        message.guild.settings.get(key)
+          .then(() => {
+            data = data.filter(g => g.key === key)[0].dataValues;
+            message.channel.send(`:information_source: \`|\` :gear: **The value of** \`${key}\` **is** \`${data.value}\`**.**`);
+          })
+          .catch(() => message.channel.send(`:x: \`|\` :gear: \`${key}\` **does not exist!** `));
+      } else {
+        var embed = new Discord.RichEmbed()
+          .setColor(client.config.colors.green)
+          .setTitle('Settings')
+          .setFooter('Guild Settings');
 
-  // Retrieve current guild settings
-  const settings = client.settings.get(message.guild.id);
+        for (settings of data) {
+          settings = settings.dataValues;
+          embed.addField(settings.key, settings.value, true);
+        }
 
-  // First, if a user does `-set add <key> <new value>`, let's add it
-  if (action === `add`) {
-    if (!key) return message.reply(`Please specify a key to add`);
-    if (settings[key]) return message.reply(`This key already exists in the settings`);
-    if (value.length < 1) return message.reply(`Please specify a value`);
-
-    // `value` being an array, we need to join it first.
-    settings[key] = value.join(` `);
-
-    // One the settings is modified, we write it back to the collection
-    client.settings.set(message.guild.id, settings);
-    message.reply(`${key} successfully added with the value of ${value.join(` `)}`);
-  } else
-
-  // Secondly, if a user does `-set edit <key> <new value>`, let's change it
-  if (action === `edit`) {
-    if (!key) return message.reply(`Please specify a key to edit`);
-    if (!settings[key]) return message.reply(`This key does not exist in the settings`);
-    if (value.length < 1) return message.reply(`Please specify a new value`);
-
-    settings[key] = value.join(` `);
-
-    client.settings.set(message.guild.id, settings);
-    message.reply(`${key} successfully edited to ${value.join(` `)}`);
-  } else
-
-  // Thirdly, if a user does `-set del <key>`, let's ask the user if they're sure...
-  if (action === `del`) {
-    if (!key) return message.reply(`Please specify a key to delete.`);
-    if (!settings[key]) return message.reply(`This key does not exist in the settings`);
-
-    // Throw the 'are you sure?' text at them.
-    const response = await client.awaitReply(message, `Are you sure you want to permanently delete ${key}? This **CANNOT** be undone.`);
-
-    // If they respond with y or yes, continue.
-    if ([`y`, `yes`].includes(response)) {
-
-      // We delete the `key` here.
-      delete settings[key];
-      client.settings.set(message.guild.id, settings);
-      message.reply(`${key} was successfully deleted.`);
+        message.channel.send(embed);
+      }
     } else
-    // If they respond with n or no, we inform them that the action has been cancelled.
-    if ([`n`,`no`,`cancel`].includes(response)) {
-      message.reply(`Action cancelled.`);
+
+    if(action === 'edit') {
+      if(!key) return message.channel.send(':x: \`|\` :gear: **You didn\'t provide a key to edit!**');
+      message.guild.settings.get(key)
+        .then(() => {
+          if(value < 1) return message.channel.send(':x: \`|\` :gear: **Please provide a new value.**');
+          message.guild.settings.edit(key, value.join(' '));
+          message.channel.send(`:white_check_mark: \`|\` :gear: \`${key}\` **was successfully edited to** \`${value.join(' ')}\`**.**`);
+        })
+        .catch(() => message.channel.send(`:x: \`|\` :gear: \`${key}\` **does not exist!** `));
+    } else
+
+    if(action === 'add') {
+      if (!key) return message.channel.send(':x: \`|\` :gear: **You didn\'t provide a key to add!**');
+      if (value < 1) return message.channel.send(':x: \`|\` :gear: **Please provide a value.**');
+      message.guild.settings.add(key, value.join(' '));
+      message.channel.send(`:white_check_mark: \`|\` :gear: \`${key}\` **was successfully added with value** \`${value.join(' ')}\`**.**`);
+    } else
+
+    if(action === 'del') {
+      if(!key) return message.channel.send(':x: \`|\` :gear: **You didn\'t provide a key to delete!**');
+      message.guild.settings.get(key)
+        .then(async () => {
+          const response = await client.awaitReply(message, `:warning: \`|\` :gear: **Are you** __***SURE***__ **you want to delete** \`${key}\`**? This CANNOT be undone!** (yes/no)`);
+          if (['yes', 'y'].includes(response)) {
+            message.guild.settings.delete(key);
+            message.channel.send(`:white_check_mark: \`|\` :gear: **Successfully deleted** \`${key}\`**.**`);
+          } else if (['no', 'n']) return message.channel.send(`:white_check_mark: \`|\` :gear: **Action cancelled.**`);
+        })
+        .catch(() => message.channel.send(`:x: \`|\` :gear: \`${key}\` **does not exist!**`));
+    } else
+
+    if(action === 'reset') {
+      if (!key) return message.channel.send(':x: \`|\` :gear: **You didn\'t provide a key to reset!**');
+      message.guild.settings.get(key)
+        .then(async () => {
+          const response = await client.awaitReply(message, `:warning: \`|\` :gear: **Are you** __***SURE***__ **you want to reset** \`${key}\`**? This CANNOT be undone!** (yes/no)`);
+          if (['yes', 'y'].includes(response)) {
+            message.guild.settings.edit(key, client.config.defaultSettings[key]);
+            message.channel.send(`:white_check_mark: \`|\` :gear: **Successfully reset** \`${key}\` **to** \`${client.config.defaultSettings[key]}\`**.**`);
+          } else if (['no', 'n']) return message.channel.send(`:white_check_mark: \`|\` :gear: **Action cancelled.**`);
+        })
+        .catch(() => message.channel.send(`:x: \`|\` :gear: \`${key}\` **does not exist!**`));
+    } else {
+      var embed = new Discord.RichEmbed()
+        .setColor(client.config.colors.green)
+        .setTitle('Settings')
+        .setFooter('Guild Settings');
+
+      for (settings of data) {
+        settings = settings.dataValues;
+        embed.addField(settings.key, settings.value, true);
+      }
+
+      message.channel.send(embed);
     }
-  } else
-
-  if (action === `get`) {
-    if (!key) return message.reply(`Please specify a key to view`);
-    if (!settings[key]) return message.reply(`This key does not exist in the settings`);
-    message.reply(`The value of ${key} is currently ${settings[key]}`);
-  } else
-
-  if (action === `reset`) {
-    client.settings.set(message.guild.id, client.config.defaultSettings);
-    message.channel.send(`All guild settings have been reset.`);
-  }
-
-  else {
-    message.channel.send(inspect(settings), {code: `js`});
-  }
+  });
 };
 
 exports.conf = {
@@ -86,5 +101,5 @@ exports.help = {
   name: `set`,
   category: `System`,
   description: `View or change settings for your server.`,
-  usage: `set <view/get/edit/reset> <key> <value>`
+  usage: `set [view/edit/add/del/reset] [key] [value]`
 };
