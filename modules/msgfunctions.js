9a936bb89edf475863131653e06a27cf6c8bf786
Akihito Mikazuki <jennasisis@gmail.com>
2018-09-02 11:10:14 -0400

    msgfunctions: fixed the sqlite error when trying to add xp

--- modules/msgfunctions.js
+++ modules/msgfunctions.js
@@ -35,15 +35,16 @@ module.exports = async (message) => {
       if(!userID) return reject(new Error('Missing userID to add xp'));
       if(!amount) return reject(new Error('Missing amount to add xp'));
       if(typeof userID !== 'string') return reject(new TypeError(`"${userID}" is not a string (must be an ID)`));
-      if(message.guild.members.get(userID) === undefined) return reject(new Error(`Could not find user "${userID}" to add xp to`));
+      if(message.guild.members.get(userID) === undefined) return reject(new Error(`Could not find member "${userID}" to add xp to`));
       if(typeof amount !== 'number') return reject(new TypeError(`"${amount}" is not a number`));
+      if(amount < 0) return reject(new RangeError(`${amount} to add must be more than 0`));
 
-      message.guild.xp.findOne({where: {user: userID}}).then(async user => {
-        if(user === null) {await message.guild.xp.create({user: userID, xp: 0}); await message.guild.xp.sync();}
-        await message.guild.xp.update({xp: user.xp + amount}, {where: {user: userID}});
-        await message.guild.xp.sync();
-        return resolve(true);
-      });
+      message.guild.xp.findOrCreate({where: {user: userID}, defaults: {xp: 0}}).then(user => {
+        user = user[0].dataValues;
+        message.guild.xp.update({xp: user.xp + amount}, {where: {user: userID}});
+        message.guild.xp.sync();
+        return resolve(user.xp);
+      }).catch(e => {return reject(new Error(e))});
     });
   }
 
@@ -57,15 +58,17 @@ module.exports = async (message) => {
       if (!userID) return reject(new Error('Missing userID to subtract xp'));
       if (!amount) return reject(new Error('Missing amount to subtract xp'));
       if (typeof userID !== 'string') return reject(new TypeError(`"${userID}" is not a string (must be an ID)`));
-      if (message.guild.members.get(userID) === undefined) return reject(new Error(`Could not find user "${userID}" to subtract xp from`));
+      if (message.guild.members.get(userID) === undefined) return reject(new Error(`Could not find member "${userID}" to subtract xp from`));
       if (typeof amount !== 'number') return reject(new TypeError(`"${amount}" is not a number`));
+      if (amount <= 0) return reject(new RangeError(`${amount} to subtract must be more than 0`));
 
-      message.guild.xp.findOne({ where: { user: userID } }).then(async user => {
-        if (user === null) { await message.guild.xp.create({ user: userID, xp: 0 }); message.guild.xp.sync(); }
-        await message.guild.xp.update({ xp: user.xp - amount }, { where: { user: userID } });
-        await message.guild.xp.sync();
-        return resolve(true);
-      });
+      message.guild.xp.findOrCreate({ where: { user: userID }, defaults: { xp: 0 } }).then(user => {
+        user = user[0].dataValues;
+        if(user.xp <= 0) return reject(new RangeError(`${userID}'s xp is 0. Cannot remove more`));
+        message.guild.xp.update({ xp: user.xp - amount }, {where: {user: userID}});
+        message.guild.xp.sync();
+        return resolve(user.xp);
+      }).catch(e => {return reject(new Error(e))})
     });
   }
 
@@ -79,15 +82,12 @@ module.exports = async (message) => {
       if (!userID) return reject(new Error('Missing userID to set xp'));
       if (!amount) return reject(new Error('Missing amount to set xp'));
       if (typeof userID !== 'string') return reject(new TypeError(`"${userID}" is not a string (must be an ID)`));
-      if (message.guild.members.get(userID) === undefined) return reject(new Error(`Could not find user "${userID}" to set xp to`));
+      if (message.guild.members.get(userID) === undefined) return reject(new Error(`Could not find member "${userID}" to set xp to`));
       if (typeof amount !== 'number') return reject(new TypeError(`"${amount}" is not a number`));
 
-      message.guild.xp.findOne({ where: { user: userID } }).then(async user => {
-        if (user === null) { await message.guild.xp.create({ user: userID, xp: 0 }); message.guild.xp.sync(); }
-        await message.guild.xp.update({ xp: amount }, { where: { user: userID } });
-        await message.guild.xp.sync();
-        return resolve(true);
-      });
+      message.guild.xp.findOrCreate({ where: { user: userID }, defaults: { xp: amount } }).then(user => {
+        return resolve(user[0].dataValues.xp);
+      }).catch(e => {return reject(new Error(e))});
     });
   }
 
@@ -97,12 +97,13 @@ module.exports = async (message) => {
    */
   message.guild.xp.get = async userID => {
     return new Promise((resolve, reject) => {
-      message.guild.xp.findOne({where: {user: userID}}).then(async user => {
-        if(user === null) {
-          await message.guild.xp.create({user: userID, xp: 0});
-        }
-        return resolve(user.xp);
-      });
+      if(!userID) return reject(new Error('Missing userID to get xp from'));
+      if(typeof userID !== 'string') return reject(new TypeError(`"${userID}" is not a string (must be ID)`));
+      if (message.guild.members.get(userID) === undefined) return reject(new Error(`Could not find member "${userID}" to get xp`));
+
+      message.guild.xp.findOrCreate({ where: { user: userID }, defaults: { xp: 0 } }).then(async user => {
+        return resolve(user[0].dataValues.xp);
+      }).catch(e => {return reject(new Error(e))});
     });
   };
 
