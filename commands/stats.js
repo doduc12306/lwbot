29ae343f27a906675985648bec923ed1c5764b5c
Akihito Mikazuki <jennasisis@gmail.com>
2018-09-03 12:43:14 -0400

    stats: added guilds and users counter, inlined everything, added timestamp

--- commands/stats.js
+++ commands/stats.js
@@ -6,11 +6,14 @@ require(`moment-duration-format`);
 exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
   message.channel.send(new Discord.RichEmbed()
     .setTitle(`\`Statistics\``)
-    .addField(`Mem Usage`, `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`)
-    .addField(`Uptime`, moment.duration(client.uptime).format(`M [months] W [weeks] D [days], H [hrs], m [mins], s [secs]`))
-    .addField(`Discord.js`, `v${version}`)
-    .addField(`Node`, process.version)
+    .addField('Guilds', client.guilds.size, true)
+    .addField('Users', client.users.size, true)
+    .addField(`Memory Usage`, `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
+    .addField(`Uptime`, moment.duration(client.uptime).format(`M [months] W [weeks] D [days], H [hrs], m [mins], s [secs]`), true)
+    .addField(`Discord.js`, `v${version}`, true)
+    .addField(`Node`, process.version, true)
     .setColor(client.config.colors.green)
+    .setTimestamp()
   );
 };
 
