b5fa3828970a8ae29929526d20c86d895fb882da
Akihito Mikazuki <jennasisis@gmail.com>
2018-09-10 19:43:30 -0400

    power: eslint

--- commands/power.js
+++ commands/power.js
@@ -1,12 +1,12 @@
 exports.run = async (client, message, args) => {// eslint-disable-line no-unused-vars
-  if(!['shutdown', 'restart', 'reboot'].includes(args[0])) return message.channel.send(':x: \`|\` :arrows_counterclockwise: **Options:** \`shutdown\` \`restart\` / \`reboot\`');
+  if(!['shutdown', 'restart', 'reboot'].includes(args[0])) return message.channel.send(':x: \`|\` :arrows_counterclockwise: **Options:** \`shutdown\` \`restart\` / reboot');
 
   if(args[0] === 'shutdown') {
     message.channel.send(':white_check_mark: \`|\` :stop_sign: **Shutting down...**');
     require('child_process').exec('pm2 stop LWBot');
   }
 
-  if(args[0] === 'restart' || args[0] === 'reboot'){
+  if(args[0] === 'restart' || args[0] === 'reboot') {
     message.channel.send(`:white_check_mark: \`|\` :arrows_counterclockwise: **${args[0].toProperCase()}ing...**`);
     require('child_process').exec('pm2 restart LWBot');
   }
@@ -16,12 +16,12 @@ exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: [],
-  permLevel: `Bot Admin`
+  permLevel: 'Bot Admin'
 };
 
 exports.help = {
-  name: `power`,
-  category: `System`,
-  description: `Restart, shut down`,
-  usage: `power <shutdown/[restart/reboot]>`
+  name: 'power',
+  category: 'System',
+  description: 'Restart, shut down',
+  usage: 'power <shutdown/[restart/reboot]>'
 };
