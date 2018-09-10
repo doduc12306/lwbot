b4806820fa6464ede14f031b4339335507fd9c41
Akihito Mikazuki <jennasisis@gmail.com>
2018-09-10 19:38:37 -0400

    guildCreate: eslint

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,17 +1,16 @@
-const Discord = require(`discord.js`);
-const Sequelize = require(`sequelize`);
+const Sequelize = require('sequelize');
 
 module.exports = async (client, guild) => {
 
   // Thanks for this PSA, York. This guild is full of userbots spamming commands. If this bot enters it, you'll see extreme performance issues.
-  if(guild.id === `439438441764356097`) return guild.leave();
+  if(guild.id === '439438441764356097') return guild.leave();
 
   // We need to add this guild to our settings!
   if(!client.settings.get(guild.id)) client.settings.set(guild.id, client.config.defaultSettings);
 
-  var guildTable = new Sequelize(`database`, `user`, `password`, {
-    host: `localhost`,
-    dialect: `sqlite`,
+  var guildTable = new Sequelize('database', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
     storage: `databases/servers/${guild.id}.sqlite`
   });
 
@@ -37,17 +36,17 @@ module.exports = async (client, guild) => {
   guild.settings.create({key: 'botCommanderRole', value: 'Bot Commander'});
   guildTable.sync();
 
-  if(!guild.me.permissions.has('SEND_MESSAGES')) guild.owner.send(':x: **CRITICAL PERMISSION MISSING:** \`Send Messages\` **WHICH EVERYTHING REQUIRES!**');
+  if(!guild.me.permissions.has('SEND_MESSAGES')) guild.owner.send(':x: **CRITICAL PERMISSION MISSING:** Send Messages **WHICH EVERYTHING REQUIRES!**');
 
   var role;
-  if(!guild.roles.find(`name`, `Muted`)) {
-    role = guild.createRole({name: `Muted`});
-    guild.channels.filter(g => g.type === `text`).forEach(channel => {channel.overwritePermissions(role, {SEND_MESSAGES: false, ADD_REACTIONS: false});});
-    guild.channels.filter(g => g.type === `voice`).forEach(channel => {channel.overwritePermissions(role, {CONNECT: false, SPEAK: false});});
+  if(!guild.roles.find('name', 'Muted')) {
+    role = guild.createRole({name: 'Muted'});
+    guild.channels.filter(g => g.type === 'text').forEach(channel => {channel.overwritePermissions(role, {SEND_MESSAGES: false, ADD_REACTIONS: false});});
+    guild.channels.filter(g => g.type === 'voice').forEach(channel => {channel.overwritePermissions(role, {CONNECT: false, SPEAK: false});});
   } else {
-    role = guild.roles.find(`name`, `Muted`) || guild.roles.find(`name`, `muted`);
-    guild.channels.filter(g => g.type === `text`).forEach(channel => {channel.overwritePermissions(role, {SEND_MESSAGES: false});});
-    guild.channels.filter(g => g.type === `voice`).forEach(channel => {channel.overwritePermissions(role, {CONNECT: false, SPEAK: false});});
+    role = guild.roles.find('name', 'Muted') || guild.roles.find('name', 'muted');
+    guild.channels.filter(g => g.type === 'text').forEach(channel => {channel.overwritePermissions(role, {SEND_MESSAGES: false});});
+    guild.channels.filter(g => g.type === 'voice').forEach(channel => {channel.overwritePermissions(role, {CONNECT: false, SPEAK: false});});
   }
 
 };
