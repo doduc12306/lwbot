ed13e8f6e1061cda9dfe3b7b68dc752967925dbb
Akihito Mikazuki <jennasisis@gmail.com>
2018-09-26 10:05:25 -0400

    stats: removed d.js and node versions

--- commands/stats.js
+++ commands/stats.js
@@ -1,4 +1,3 @@
-const { version } = require('discord.js');
 const moment = require('moment');
 const Discord = require('discord.js');
 require('moment-duration-format');
@@ -10,8 +9,6 @@ exports.run = (client, message, args, level) => { // eslint-disable-line no-unus
     .addField('Users', client.users.size, true)
     .addField('Memory Usage', `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
     .addField('Uptime', `:robot: ${moment.duration(client.uptime).format('M [months] W [weeks] D [days], H [hrs], m [mins], s [secs]')}\n :desktop: ${require('child_process').execSync('uptime -p').toString().split('up')[1].trim()}`, true)
-    .addField('Discord.js', `v${version}`, true)
-    .addField('Node', process.version, true)
     .setColor(client.config.colors.green)
     .setTimestamp()
   );
