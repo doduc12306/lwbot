806b461c75edf5a67b3e90468cdcb6d5d9c4f6f5
Akihito Mikazuki <jennasisis@gmail.com>
2018-10-10 21:18:53 -0400

    channelCreate: made sure roles dont duplicate

--- src/events/channelCreate.js
+++ src/events/channelCreate.js
@@ -1,14 +1,12 @@
-module.exports = (client, channel) => {
+module.exports = async (client, channel) => {
   if(channel.type === 'dm') return;
 
-  var role = channel.guild.roles.find('name', 'Muted') || channel.guild.roles.find('name', 'muted');
-
-  if(!role) {
-    role = channel.guild.createRole({name: 'Muted'});
-    channel.guild.channels.forEach(g => {
-      if(g.type === 'text') g.overwritePermissions(role.id, {SEND_MESSAGES: false, ADD_REACTIONS: false});
-      if(g.type === 'voice') g.overwritePermissions(role.id, {CONNECT: false, SPEAK: false});
-    });
+  var role = await channel.guild.roles.find(role => role.name === 'Muted');
+  if (role === null) {
+    role = await channel.guild.roles.find(role => role.name === 'muted');
+    if (role === null) {
+      role = await channel.guild.createRole({ name: 'Muted', color: 'ORANGE' });
+    }
   }
 
   if(channel.type === 'text') return channel.overwritePermissions(role.id, {SEND_MESSAGES: false, ADD_REACTIONS: false});
