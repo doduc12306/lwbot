d74457e919e25f0d0116852676bc9b6294d4d3b4
Akihito Mikazuki <jennasisis@gmail.com>
2018-10-20 09:58:54 -0400

    Added webtoon central addons

---
+++ modules/wtc.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('../src/config.js');
+const Sequelize = require('sequelize');
+
+var killList = new Sequelize('database', 'user', 'password', {
+  host: 'localhost',
+  dialect: 'sqlite',
+  logging: false,
+  storage: '../src/databases/killList.sqlite'
+});
+killList = killList.define('killList', {
+  user: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+killList.sync();
+
+const prefix = '~wa ';
+
+if (process.argv.includes('-d') || process.argv.includes('--debug')) client.login(config.debugtoken);
+else client.login(config.token);
+
+client.on('ready', () => console.log(`Wtc addon ready: ${client.user.tag}`));
+
+client.on('message', message => {
+  if(message.author.bot || message.author === client.user) return;
+  if(!message.content.startsWith(prefix)) return;
+  if (message.guild.id !== '381192127050153993') return message.channel.send(':x: `|` **This guild is not authorized to use any of these commands!**');
+
+  if(message.content === `${prefix}ping`) return message.channel.send(`:ping_pong: ${Math.round(client.ping)}ms`);
+
+  if (message.content.startsWith(`${prefix}kill`)) {
+    if (!['420220760066490379', '107599228900999168'].includes(message.author.id)) return message.channel.send(':x: `|` **Only chair can access this command!**');
+    if (message.mentions.members.size === 0) return message.channel.send(':x: `|` **You didn\'t mention anyone to kill!**');
+
+    message.mentions.members.forEach(async mention => {
+      var role = '497871607692263425';
+      if (!mention.roles.has(role)) return message.channel.send(`:x: \`|\` **${mention.user.tag} didn't have the Murder Mystery role. No action was taken.**`);
+      await killList.findOrCreate({where: {user: mention.user.id}})
+        .then(() => {
+          mention.removeRole(role);
+          message.channel.send(`:white_check_mark: \`|\` **Removed Murder Mystery from \`${mention.user.tag}\` and added to killed list.**`);
+        })
+        .catch(e => message.channel.send(`<@107599228900999168> :x: **Error adding user to kill db:** \`${e}\``));
+      await setTimeout(() => { }, 1000);
+    });
+  }
+});
+
+/* client.on('guildMemberAdd', member => {
+  if(member.guild.id !== '381192127050153993' || member.guild.id !== '444250305618509824') return;
+
+  if(member.displayName.match(/.gg\//gm)) message.guild.ban(member.user, {reason: '[Auto Ban] Detected .gg in username'});
+}); */
