77835c0105620e0e06a81295a3d175e0be636321
Akihito Mikazuki <jennasisis@gmail.com>
2018-10-28 14:44:46 -0400

    wtc: various changes (see desc) * Extends 4a2e44aa * Checks if message guild ID is one of three main * Added a dead command: returns all dead users * guildMemberAdd: bans anyone (in specified guilds) that joins with a .gg username

--- modules/wtc.js
+++ modules/wtc.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('../src/config.js');
 const Sequelize = require('sequelize');
+
+var { join } = require('path');
+require('dotenv').config({ path: join(__dirname, '../.env') });
 
 var killList = new Sequelize('database', 'user', 'password', {
   host: 'localhost',
@@ -19,20 +21,20 @@ killList.sync();
 
 const prefix = '~wa ';
 
-if (process.argv.includes('-d') || process.argv.includes('--debug')) client.login(config.debugtoken);
-else client.login(config.token);
+if (process.argv.includes('-d') || process.argv.includes('--debug')) client.login(process.env.DEBUG_TOKEN);
+else client.login(process.env.TOKEN);
 
 client.on('ready', () => console.log(`Wtc addon ready: ${client.user.tag}`));
 
-client.on('message', message => {
+client.on('message', async message => {
   if(message.author.bot || message.author === client.user) return;
   if(!message.content.startsWith(prefix)) return;
-  if (message.guild.id !== '381192127050153993') return message.channel.send(':x: `|` **This guild is not authorized to use any of these commands!**');
+  if (!['381192127050153993', '444250305618509824', '332632603737849856'].includes(message.guild.id)) return message.channel.send(':x: `|` **This guild is not authorized to use any of these commands!**');
 
   if(message.content === `${prefix}ping`) return message.channel.send(`:ping_pong: ${Math.round(client.ping)}ms`);
 
   if (message.content.startsWith(`${prefix}kill`)) {
-    if (!['420220760066490379', '107599228900999168'].includes(message.author.id)) return message.channel.send(':x: `|` **Only chair can access this command!**');
+    if (!['420220760066490379', '107599228900999168'].includes(message.author.id)) return message.channel.send(':x: `|` **Only chair / James can access this command!**');
     if (message.mentions.members.size === 0) return message.channel.send(':x: `|` **You didn\'t mention anyone to kill!**');
 
     message.mentions.members.forEach(async mention => {
@@ -47,10 +49,21 @@ client.on('message', message => {
       await setTimeout(() => { }, 1000);
     });
   }
+
+  if(message.content === `${prefix}dead`) {
+    var deadList = [];
+    killList.findAll().then(async dead => {
+      for(var users of dead) deadList.push(client.users.get(users.dataValues.user));
+      message.channel.send(new Discord.RichEmbed()
+        .setColor(require('../src/config').colors.red)
+        .setDescription(deadList.join(' '))
+      );
+    });
+  }
 });
 
-/* client.on('guildMemberAdd', member => {
-  if(member.guild.id !== '381192127050153993' || member.guild.id !== '444250305618509824') return;
+client.on('guildMemberAdd', member => {
+  if (!['381192127050153993', '444250305618509824'].includes(member.guild.id)) return;
 
-  if(member.displayName.match(/.gg\//gm)) message.guild.ban(member.user, {reason: '[Auto Ban] Detected .gg in username'});
-}); */
+  if(member.displayName.match(/.gg/gm)) member.guild.ban(member.user, {reason: '[Auto Ban] Detected .gg in username'});
+});
