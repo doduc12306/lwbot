52c0791fac05d8e4fd1b94721fc9b0b228e0204f
Akihito Mikazuki <jennasisis@gmail.com>
2018-10-28 14:46:33 -0400

    guildCreate: handles errors gracefully sends an error message to the guild owner if shit went wrong during setup

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 
 module.exports = async (client, guild) => {
-
   // Thanks for this PSA, York. This guild is full of userbots spamming commands. If this bot enters it, you'll see extreme performance issues.
   if(guild.id === '439438441764356097') return guild.leave();
   client.logger.log(`Joined guild ${guild.name} (${guild.id})`);
@@ -9,7 +8,8 @@ module.exports = async (client, guild) => {
   var guildTable = new Sequelize('database', 'user', 'password', {
     host: 'localhost',
     dialect: 'sqlite',
-    storage: `databases/servers/${guild.id}.sqlite`
+    storage: `databases/servers/${guild.id}.sqlite`,
+    logging: false
   });
 
   // Guild settings intialization
@@ -35,14 +35,30 @@ module.exports = async (client, guild) => {
   if (!guild.me.permissions.has('SEND_MESSAGES')) guild.owner.send(':x: **CRITICAL PERMISSION MISSING:** `Send Messages` **WHICH EVERYTHING REQUIRES!**');
   if (!guild.me.permissions.has('EMBED_LINKS')) guild.owner.send(':x: **CRITICAL PERMISSION MISSING:** `Embed Links` **WHICH EVERYTHING REQUIRES!**');
 
+  var textErrored = false;
+  var voiceErrored = false;
+  var mutedRoleCreateError = false;
+
   var role = await guild.roles.find(role => role.name === 'Muted');
   if(role === null) {
     role = await guild.roles.find(role => role.name === 'muted');
     if(role === null) {
-      role = await guild.createRole({name: 'Muted', color: 'ORANGE'});
+      role = await guild.createRole({name: 'Muted', color: 'ORANGE'}, 'Initial Setup Process: Created role to add channel permissions with').catch(() => mutedRoleCreateError = true);
     }
   }
-  await guild.channels.filter(g => g.type === 'text').forEach(channel => {channel.overwritePermissions(role, {SEND_MESSAGES: false, ADD_REACTIONS: false});});
-  await guild.channels.filter(g => g.type === 'voice').forEach(channel => {channel.overwritePermissions(role, {CONNECT: false, SPEAK: false});});
 
+  await guild.channels.filter(g => g.type === 'text').forEach(channel => {
+    channel.overwritePermissions(role, {SEND_MESSAGES: false, ADD_REACTIONS: false}, 'Initial Setup Process').catch(() => textErrored = true);
+  });
+  await guild.channels.filter(g => g.type === 'voice').forEach(channel => {
+    channel.overwritePermissions(role, {CONNECT: false, SPEAK: false}, 'Inital Setup Process').catch(() => voiceErrored = true);
+  });
+
+  if(mutedRoleCreateError) {
+    var msg = ':x: **Something went wrong during my setup process.**\n`-` I was unable to create a Muted role.';
+    if(textErrored) msg += '\n`-` I was unable to disallow the Muted role from talking in text channels.';
+    if(voiceErrored) msg += '\n`-` I was unable to disallow the Muted role from talking in voice channels.';
+    msg += '\n\n**Please re-invite me.** When you invite me, please make sure I have all the permissions indicated on the invite page.';
+    guild.owner.send(msg);
+  }
 };
