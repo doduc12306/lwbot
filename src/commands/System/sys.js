e1cb71242dd5922cbf6d78f6cef1516c1470fa16
Akihito Mikazuki <jennasisis@gmail.com>
2018-11-10 15:09:31 -0500

    sys: completely reworked. much better

--- src/commands/System/sys.js
+++ src/commands/System/sys.js
@@ -1,12 +1,36 @@
-module.exports.run = (client, message, args) => {
+const { post } = require('snekfetch');
+
+module.exports.run = async (client, message, args) => {
+  const code = args.join(' ');
+  const token = client.token.split('').join('[^]{0,2}');
+  const rev = client.token.split('').reverse().join('[^]{0,2}');
+  const filter = new RegExp(`${token}|${rev}`, 'g');
   try {
-    require('child_process')
-      .exec(args.join(' '), (e, out, err) => {
-        if (e) return message.channel.send(`\`\`\`xl\n${e}\n\`\`\``);
-        if (err) return message.channel.send(`\`\`\`xl\n${err}\n\`\`\``);
-        message.channel.send(`\`\`\`xl\n${out}\n\`\`\``);
-      });
-  } catch (e) {message.channel.send(`\`\`\`xl\n${e}\n\`\`\``);}
+    require('child_process').exec(code, async (e, out, err) => {
+      if(e || err) return message.channel.send(e || err, {code: 'xl'});
+      out = out.replace(filter, 'FILTERED TOKEN');
+      out = clean(out);
+      if (out.length < 1950) {
+        message.channel.send(out, {code: 'bash'});
+      } else {
+        try {
+          const { body } = await post('https://www.hastebin.com/documents').send(out);
+          message.channel.send(`:x: **Output too long, uploaded to hastebin:** https://www.hastebin.com/${body.key}.js `);
+        } catch (error) {
+          message.channel.send(`:x: **Hastebin upload error:** \`${error.name}\`\n\`\`\`\n${error.message}\n\`\`\``);
+        }
+      }
+    });
+  } catch (error) {
+    message.channel.send(`\`\`\`xl\n${error}\n\`\`\``);
+  }
+
+  function clean(text) {
+    return text
+      .replace(/`/g, '`' + String.fromCharCode(8203))
+      .replace(/@/g, '@' + String.fromCharCode(8203))
+      .replace(/(\[\w+)|(\1;\d+m)/g, String.fromCharCode(8203));
+  }
 };
 
 exports.conf = {
