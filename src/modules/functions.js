bbd69adbe280f00d126e69566228c8496ec5283a
Akihito Mikazuki <jennasisis@gmail.com>
2018-11-30 17:46:03 -0500

    functions: changed .random to .randomElement

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -228,7 +228,7 @@ module.exports = (client) => {
 
   // <Array>.random() returns a single random element from an array
   // [1, 2, 3, 4, 5].random() can return 1, 2, 3, 4 or 5.
-  Array.prototype.random = function() {
+  Array.prototype.randomElement = function() {
     return this[Math.floor(Math.random() * this.length)];
   };
 
