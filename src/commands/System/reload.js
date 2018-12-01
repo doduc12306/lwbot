838f3a65c85eb12b26a4ede0c5e5ddf1e9f48871
Akihito Mikazuki <jennasisis@gmail.com>
2018-12-01 15:38:13 -0500

    reload: added file reload

--- src/commands/System/reload.js
+++ src/commands/System/reload.js
@@ -1,6 +1,13 @@
 exports.run = async (client, message, args) => {
-  if (!args[0]) {
-    message.channel.send(':x: You didn\'t give me a command to reload!');
+  if (!args[0]) return message.channel.send(':x: You didn\'t give me a command to reload!');
+  if (args[0] === 'file') {
+    try {
+      delete require.cache[require.resolve(args[1])];
+      message.channel.send(`:white_check_mark: **Reloaded** \`${args[1]}\``);
+      client.logger.log('Reloaded'+args[1]);
+    } catch (err) {
+      return message.channel.send(`:x: \`${err}\``);
+    }
   } else {
     let response = await client.unloadCommand(args[0]);
     if (response) return message.channel.send(`:x: **Error Unloading:** ${response}`);
