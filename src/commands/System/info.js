3792ea45f349b53bd578aac9254c73f622b7982c
jennasisis <jennasisis@gmail.com>
2019-01-12 20:09:33 -0500

    info: fixed package path finding

--- src/commands/System/info.js
+++ src/commands/System/info.js
@@ -5,7 +5,7 @@ module.exports.run = (client, message) => {
     .setColor('0x59D851')
     .setAuthor('LINE WEBTOON', client.user.avatarURL)
     .setThumbnail(client.user.avatarURL)
-    .addField('Version', require('../package.json').version, true)
+    .addField('Version', require('../../../package.json').version, true)
     .addField('Library', '[Discord.js](http://discord.js.org/)', true)
     .addField('Creators', '<@107599228900999168>', true)
     .addField('Honorable Mentions', '[`An Idiot\'s Guide`](http://anidiots.guide/) - Command handler, eval command, reload command, and so many other things. Thank you *so* much.\n[`discordjs.guide`](http://discordjs.guide) - Provided tagging system.\n[`Discord.js Server`](https://discord.gg/bRCvFy9) - You guys answered every question the docs couldn\'t.')
