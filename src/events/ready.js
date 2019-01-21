6d3360963e133f1bf7ec71be646f64f5e7824f7a
Akihito Mikazuki <jennasisis@gmail.com>
2019-01-21 14:08:00 -0500

    ready: changed some stuff mainly, the error log works now.

--- src/events/ready.js
+++ src/events/ready.js
@@ -14,14 +14,16 @@ module.exports = async client => {
   await Promise.all(servers.map(async (server) => {
     const serverId = server.split('.sqlite')[0];
     const db = new Sequelize('database', 'username', 'password', {logging: false, host: 'localhost', storage: `databases/servers/${server}`, dialect: 'sqlite'});
-    client.logger.debug(`Opened server ${server}`);
+    client.logger.verbose(`Opened server ${server}`);
+    if(!server.endsWith('.sqlite')) return client.logger.warn('Non-sqlite file found in databases/servers! File: ' + server);
+    if(!/[1-9]+/g.test(server)) client.logger.warn('Non-server file found in databases/servers! File: ' + server);
     const [data] = await db.query('SELECT * FROM \'settings\'');
     const settings = {};
     data.forEach(({ key, value }) => {
       settings[key] = value;
     });
     client.settings.set(serverId, settings);
-    client.logger.debug(`Mapped settings for ${server}`);
+    client.logger.verbose(`Mapped settings for ${server}`);
   }));
 
   const after = new Date();
@@ -31,13 +33,13 @@ module.exports = async client => {
   client.logger.log(`${client.user.tag} | ${client.users.size} Users | ${client.guilds.size} Guilds | Took ${client.startup}ms`, 'ready');
   if(client.config.debugMode) client.logger.warn('Debug mode enabled');
 
-  // Finds if there was an error generated on unhandledRejection the last time the bot started up.
+  // Finds if there was an error generated on uncaughtException the last time the bot started up.
   // This is achieved by writing a new file on error, exiting, then on restart, reading the file
   // then sending the contents to me.
   try {
     const fs = require('fs');
     let e = require.resolve('../e');
-    e = fs.readFileSync('e', 'utf8');
+    e = await fs.readFileSync('e', 'utf8');
     await client.users.get(client.config.ownerID).send(`**I restarted! There was an error before I restarted:**\n\`\`\`${e}\`\`\``);
     await fs.unlink('e', (err) => {
       if(err) throw err;
