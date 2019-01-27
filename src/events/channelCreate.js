bffa774209f2ef0668c3661876250771e2b40b0d
jennasisis <jennasisis@gmail.com>
2019-01-27 14:09:07 -0500

    channelCreate: e f f i c i e n c y

--- src/events/channelCreate.js
+++ src/events/channelCreate.js
@@ -1,13 +1,8 @@
 module.exports = async (client, channel) => {
   if(channel.type === 'dm') return;
 
-  let role = await channel.guild.roles.find(role => role.name === 'Muted');
-  if (role === null) {
-    role = await channel.guild.roles.find(role => role.name === 'muted');
-    if (role === null) {
-      role = await channel.guild.createRole({ name: 'Muted', color: 'ORANGE' });
-    }
-  }
+  let role = await channel.guild.roles.find(g => g.name.toLowerCase() === 'muted');
+  if (role === null) role = await channel.guild.createRole({ name: 'Muted', color: 'DARK_ORANGE', position: channel.guild.me.highestRole.position - 1 }, 'Guild setup!').catch(() => {});
 
   if(channel.type === 'text') return channel.overwritePermissions(role.id, {SEND_MESSAGES: false, ADD_REACTIONS: false});
   if(channel.type === 'voice') return channel.overwritePermissions(role.id, {CONNECT: false, SPEAK: false});
