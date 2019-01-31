2639982a0a82e785b6d0fbd029fc2ead29f8d742
Akihito Mikazuki <jennasisis@gmail.com>
2019-01-30 22:02:23 -0500

    message: see desc * removed the getRandomIntInclusive function and just put it in raw (twss) to the xp add function * caps limit support (database calling/creating) * if the command is disabled BUT level 10 calls it, run the command anyway * eslint formatting * general aethsetic frontend formatting

--- src/events/message.js
+++ src/events/message.js
@@ -6,24 +6,52 @@ module.exports = async (client, message) => {
   if (message.author.bot) return;
   require('../modules/msgfunctions.js')(client, message);
 
-  // Thanks, MDN
-  function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  if(message.channel.type !== 'dm') {
-    if(!client.xpLockSet.has(message.author.id)) {
-      message.guild.xp.add(message.author.id, getRandomIntInclusive(1, 2));
+  if (message.channel.type !== 'dm') {
+    if (!client.xpLockSet.has(message.author.id)) {
+      message.guild.xp.add(message.author.id, Math.floor(Math.random() * (Math.floor(2) - Math.ceil(1) + 1)) + Math.ceil(1)); // Adds either 1 or 2 xp ...
       client.xpLockSet.add(message.author.id);
-      setTimeout(() => client.xpLockSet.delete(message.author.id), 60000);
+      setTimeout(() => client.xpLockSet.delete(message.author.id), 60000); // ... per minute.
     }
+  }
+
+  let capsWarnEnabled = client.config.defaultSettings.capsWarnEnabled;
+  if (!message.guild) capsWarnEnabled = client.config.defaultSettings.capsWarnEnabled;
+  else {
+    await message.guild.settings.findOrCreate({ where: { key: 'capsWarnEnabled' }, defaults: { value: 'false' } });
+    capsWarnEnabled = await message.guild.settings.get('capsWarnEnabled');
+  }
+
+  let capsThreshold = client.config.defaultSettings.capsThreshold;
+  if (!message.guild) capsThreshold = client.config.defaultSettings.capsThreshold;
+  else {
+    await message.guild.settings.findOrCreate({ where: { key: 'capsThreshold' }, defaults: { value: '70' } });
+    capsThreshold = await message.guild.settings.get('capsThreshold');
+  }
+
+  let staffBypassesLimits = client.config.defaultSettings.staffBypassesLimits;
+  if (!message.guild) staffBypassesLimits = client.config.defaultSettings.staffBypassesLimits;
+  else {
+    await message.guild.settings.findOrCreate({ where: { key: 'staffBypassesLimits' }, defaults: { value: 'true' } });
+    staffBypassesLimits = await message.guild.settings.get('staffBypassesLimits');
+  }
+
+  const exceedsCapsThreshold = message.content.match(/[A-Z]+/g) !== null &&
+    message.content.length >= 15 &&
+    capsWarnEnabled === 'true' &&
+    (message.content.match(/[A-Z]+/g).join(' ').replaceAll(' ', '').split('').length / message.content.length) * 100 >= capsThreshold; // ok so this mess of code checks to see if the message has more than ${capsThreshold}% caps. To break it down, it matches all of the capital letters in the message. Then joins the array that spits out. Then it replaces all of the spaces with an empty string, so they looklikethisinsteadofspaced, then it splits it from each character. It takes the length of this array and divides it by the length of the message itself. It then multiplies that number by 100 to give the percentage, then it checks that number against ${capsThreshold}.
+
+  const emsg = `:warning: \`|\` ${message.author}**, your message is more than ${capsThreshold}% caps.** Please do not spam caps.`;
+  if(exceedsCapsThreshold) {
+    if (client.permlevel(message) !== 0) {
+      if(staffBypassesLimits === 'true');
+      else { message.delete(); message.send(emsg).then(msg => msg.delete(6000)); }
+    } else { message.delete(); message.send(emsg).then(msg => msg.delete(6000)); }
   }
 
   let prefix = client.config.defaultSettings.prefix;
   if (!message.guild) prefix = client.config.defaultSettings.prefix;
   else {
-    await message.guild.settings.findOrCreate({where: {key: 'prefix'}, defaults: {value: '!w '}});
+    await message.guild.settings.findOrCreate({ where: { key: 'prefix' }, defaults: { value: '!w ' } });
     prefix = await message.guild.settings.get('prefix');
   }
 
@@ -46,9 +74,9 @@ module.exports = async (client, message) => {
   }
   // using this const varName = thing OR otherthing; is a pretty efficient
   // and clean way to grab one of 2 values!
-  if (!cmd) return message.send(`:x: That isn't one of my commands! Try ${prefix}help`);
+  if (!cmd) return message.send(`:x: **That isn't one of my commands!** Try \`${prefix}help\``);
 
-  if (!cmd.conf.enabled) if (systemNotice === 'true') return message.send(`:x: \`${cmd.help.name}\` **is currently disabled.**`);
+  if(!cmd.conf.enabled && systemNotice === 'true' && message.author.id !== client.config.ownerID) return message.send(`:x: **The command** \`${cmd}\` **is currently disabled.**`);
 
   // Some commands may not be useable in DMs. This check prevents those commands from running
   // and return a friendly error message.
@@ -71,14 +99,21 @@ module.exports = async (client, message) => {
 
   // Other server database checks
   if (message.guild) {
-    await message.guild.settings.findOrCreate({ where: { key: 'modLogChannel' }, defaults: { value: 'mod_logs' } });
-    await message.guild.settings.findOrCreate({ where: { key: 'modRole' }, defaults: { value: 'Mods' } });
-    await message.guild.settings.findOrCreate({ where: { key: 'adminRole' }, defaults: { value: 'Admins' } });
-    await message.guild.settings.findOrCreate({ where: { key: 'welcomeEnabled' }, defaults: { value: 'true' } });
-    await message.guild.settings.findOrCreate({ where: { key: 'welcomeChannel' }, defaults: { value: 'welcome' } });
-    await message.guild.settings.findOrCreate({ where: { key: 'welcomeMessage' }, defaults: { value: 'Welcome to the server, {{user}}!' } });
-    await message.guild.settings.findOrCreate({ where: { key: 'announcementChannel' }, defaults: { value: 'announcements' } });
-    await message.guild.settings.findOrCreate({ where: { key: 'botCommanderRole' }, defaults: { value: 'Bot Commander' } });
-    await message.guild.settings.findOrCreate({ where: { key: 'ownerRole' }, defaults: { value: 'Owners' } });
+    message.guild.settings.findOrCreate({ where: { key: 'modLogChannel' }, defaults: { value: 'mod_logs' } });
+    message.guild.settings.findOrCreate({ where: { key: 'modRole' }, defaults: { value: 'Mods' } });
+    message.guild.settings.findOrCreate({ where: { key: 'adminRole' }, defaults: { value: 'Admins' } });
+    message.guild.settings.findOrCreate({ where: { key: 'botCommanderRole' }, defaults: { value: 'Bot Commander' } });
+    message.guild.settings.findOrCreate({ where: { key: 'ownerRole' }, defaults: { value: 'Owners' } });
+
+    message.guild.settings.findOrCreate({ where: { key: 'welcomeEnabled' }, defaults: { value: 'true' } });
+    message.guild.settings.findOrCreate({ where: { key: 'welcomeChannel' }, defaults: { value: 'welcome' } });
+    message.guild.settings.findOrCreate({ where: { key: 'welcomeMessage' }, defaults: { value: 'Welcome to the server, {{user}}!' } });
+
+    message.guild.settings.findOrCreate({ where: { key: 'announcementChannel' }, defaults: { value: 'announcements' } });
+
+    message.guild.settings.findOrCreate({ where: { key: 'capsWarnEnabled' }, defaults: { value: 'false' } });
+    message.guild.settings.findOrCreate({ where: { key: 'capsThreshold' }, defaults: { value: '70' } });
+
+    message.guild.settings.findOrCreate({ where: { key: 'staffBypassesLimits' }, defaults: { value: 'true' } });
   }
 };
