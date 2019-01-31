bc352df6de107cc5e638c43c0049d39f87ae9b6e
Akihito Mikazuki <jennasisis@gmail.com>
2019-01-30 21:59:18 -0500

    ratelimit: pm2 detection

--- src/events/ratelimit.js
+++ src/events/ratelimit.js
@@ -2,5 +2,6 @@ module.exports = (client, rateLimitInfo) => {
   client.logger.error('Ratelimited!');
   console.error(rateLimitInfo);
   client.logger.error('Process will shut down now.');
-  process.exit(1);
+  if(process.env._pm2_version) return require('child_process').exec('pm2 stop LWBot');
+  else process.exit(1);
 };
