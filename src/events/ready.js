9655213b9038d9df3c09f8af649b59574532ad65
Akihito Mikazuki <jennasisis@gmail.com>
2019-01-30 22:10:35 -0500

    ready: froth it dan

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 const { statuses } = require('../util/statuses');
 const { promisify } = require('util');
 const readdir = promisify(require('fs').readdir);
@@ -33,6 +34,22 @@ module.exports = async client => {
   client.logger.log(`${client.user.tag} | ${client.users.size} Users | ${client.guilds.size} Guilds | Took ${client.startup}ms`, 'ready');
   if(client.config.debugMode) client.logger.warn('Debug mode enabled');
   if(client.config.verboseMode) client.logger.warn('Verbose mode enabled');
+
+  await client.wait(5000);
+
+  client.verbose(`
+
+      ______ ______  _____  _____  _   _    _____  _____   ______   ___   _   _
+      |  ___|| ___ \|  _  ||_   _|| | | |  |_   _||_   _|  |  _  \ / _ \ | \ | |
+      | |_   | |_/ /| | | |  | |  | |_| |    | |    | |    | | | |/ /_\ \|  \| |
+      |  _|  |    / | | | |  | |  |  _  |    | |    | |    | | | ||  _  || . ' |
+      | |    | |\ \ \ \_/ /  | |  | | | |   _| |_   | |    | |/ / | | | || |\  |
+      \_|    \_| \_| \___/   \_/  \_| | _/  \___/   \_/    |___/  \_| |_/\_| \_/
+
+
+  `);
+
+
 
   // Finds if there was an error generated on uncaughtException the last time the bot started up.
   // This is achieved by writing a new file on error, exiting, then on restart, reading the file
