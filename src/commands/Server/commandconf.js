1387b0bc136a2dedb8ed1fd3f136f1750bd559f1
Akihito Mikazuki <jennasisis@gmail.com>
2019-02-27 16:24:46 -0500

    commandconf: worked out bugs * Added validation for command presence in invoker command * Added validation for command presence in client.commands * Instead of a set of perms the user can set, I made it so that they can only set perms below theirs (line 23) * Removed the 0 and 1 enabled properties, sequelize handles that for me (it ended up erroring anyway) * Changed command permlevel to Administrator instead of Bot Commander because god knows barely any server have that

--- src/commands/Server/commandconf.js
+++ src/commands/Server/commandconf.js
@@ -1,33 +1,30 @@
 module.exports.run = async (client, message, [option, command, ...permlevel]) => {
   require('../../modules/message/commands.js')(client, message);
 
-  if(!option || !['enable', 'disable', 'setperm'].includes(option)) return message.send(':x: `|` :gear: **Invalid option!** Accepted values: `enable`, `disable`, or `setperm`');
+  if (!option || !['enable', 'disable', 'setperm'].includes(option)) return message.send(':x: `|` :gear: **Invalid option!** Accepted values: `enable`, `disable`, or `setperm`');
+  if (!command) return message.send(':x: `|` :gear: **You didn\'t give a command to edit!**');
 
   const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
-  if(!cmd) return message.send(`:x: \`|\` :gear: \`${command}\` **does not exist, nor is it an alias!**`);
-  if(!cmd.conf.enabled) return message.send(`:x: \`|\` :gear: \`${command}\` **has been disabled globally by the developer!**`);
+  if (!cmd) return message.send(`:x: \`|\` :gear: \`${command}\` **does not exist, nor is it an alias!**`);
+  if (!cmd.conf.enabled) return message.send(`:x: \`|\` :gear: \`${command}\` **has been disabled globally by the developer!**`);
 
-  if (client.levelCache[cmd.conf.permLevel] > 5 && client.permlevel(message.member) < 6) return message.send(`:x: \`|\` :gear: \`${command}\` **can only be edited by** \`Bot Admin\` **or higher!** (Permission set by the developer)`); // 5 = Server Owner | 6 = Server Owner+ (Bot Admin / Support / Owner)
+  if (['commandconf'].includes(cmd.help.name)) return message.send(`:x: \`|\` :gear: \`${command}\` **cannot be edited!**`); // A list of commands that cannot be edited by ANYONE
 
-  if(['commandconf'].includes(cmd.help.name)) return message.send(`:x: \`|\` :gear: \`${command}\` **cannot be disabled!**`); // A list of commands that cannot be edited by ANYONE
-
-  if(['commandconf', 'eval', 'sys', 'power', 'registercmd', 'reload', 'set'].includes(cmd.help.name) && client.permlevel(message.member) > 5) return message.send('oh come *on* dude, you\'re a trusted member of my team. you trying to disable a vital command does *not* help. if it *really* needs to be disabled, get me to do it through the database or something.\n- Akii'); // Basically bot helper shaming, then redirecting to me.
+  if (['commandconf', 'eval', 'sys', 'power', 'registercmd', 'reload', 'set'].includes(cmd.help.name) && client.permlevel(message.member) > 5) return message.send('oh come *on* dude, you\'re a trusted member of my team. you trying to disable a vital command does *not* help. if it *really* needs to be disabled, get me to do it through the database or something.\n- Akii'); // Basically bot helper shaming, then redirecting to me.
 
   let cmdInDb;
   if (message.guild) {
     await message.guild.commands.findOne({ where: { command: command } }).then(data => {
       if (!data) return message.send(`:x: \`|\` :gear: \`${command}\` **has been disabled globally by the developer!**`);
-      else {
-        if (data.dataValues.enabled === '0') data.dataValues.enabled = false;
-        else data.dataValues.enabled = true;
-        cmdInDb = data.dataValues;
-      }
+      else cmdInDb = data.dataValues;
     });
   }
 
-  switch(option.toLowerCase()) {
+  if (client.permlevel(message.member) < client.levelCache[cmdInDb.permLevel]) return message.send(`:x: \`|\` :gear: \`${command}\`**'s permission level is higher than yours!**`);
+
+  switch (option.toLowerCase()) {
     case 'enable': {
-      if(cmdInDb.enabled) return message.send(`:x: \`|\` :gear: \`${command}\` **is already enabled!**`);
+      if (cmdInDb.enabled) return message.send(`:x: \`|\` :gear: \`${command}\` **is already enabled!**`);
       message.guild.commands.update({ enabled: true }, { where: { command: command } })
         .then(() => message.send(`:white_check_mark: \`|\` :gear: \`${command}\` **is now enabled!**`))
         .catch(e => {
@@ -37,7 +34,7 @@ module.exports.run = async (client, message, [option, command, ...permlevel]) =>
       break;
     }
     case 'disable': {
-      if(!cmdInDb.enabled) return message.send(`:x: \`|\` :gear: \`${command}\` **is already disabled!**`);
+      if (!cmdInDb.enabled) return message.send(`:x: \`|\` :gear: \`${command}\` **is already disabled!**`);
       message.guild.commands.update({ enabled: false }, { where: { command: command } })
         .then(() => message.send(`:white_check_mark: \`|\` :gear: \`${command}\` **is now disabled!**`))
         .catch(e => {
@@ -47,7 +44,7 @@ module.exports.run = async (client, message, [option, command, ...permlevel]) =>
       break;
     }
     case 'setperm': {
-      if(!permlevel || !['User', 'Moderator', 'Administrator', 'Bot Commander', 'Server Owner'].includes(permlevel)) return message.send(':x: `|` :gear: **Invalid permission level!** Accepted values: `User`, `Moderator`, `Administrator`, `Bot Commander`, or `Server Owner`'); // Type validation
+      if (!permlevel || !['User', 'Moderator', 'Administrator', 'Bot Commander', 'Server Owner'].includes(permlevel)) return message.send(':x: `|` :gear: **Invalid permission level!** Accepted values: `User`, `Moderator`, `Administrator`, `Bot Commander`, or `Server Owner`'); // Type validation
 
       message.guild.commands.update({ permLevel: permlevel }, { where: { command: command } })
         .then(() => message.send(`:white_check_mark: \`|\` :gear: \`${command}\`**'s permission level has been updated to ${permlevel}.**`))
@@ -66,7 +63,7 @@ exports.conf = {
   enabled: true,
   aliases: ['command', 'changecmd', 'changecommand', 'cmdconf'],
   guildOnly: true,
-  permLevel: 'Bot Commander'
+  permLevel: 'Administrator'
 };
 
 exports.help = {
