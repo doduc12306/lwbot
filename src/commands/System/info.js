d1e1a9935a650a3f1773353f3e830b9d0ff0c949
jennasisis <jennasisis@gmail.com>
2019-04-22 21:07:06 -0400

    info: cleanup, added d.js version

--- src/commands/System/info.js
+++ src/commands/System/info.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
-
+const package = require('../../../package.json');
 module.exports.run = (client, message) => {
   message.send(new Discord.RichEmbed()
-    .setColor('0x59D851')
+    .setColor(client.config.colors.green)
     .setAuthor('LINE WEBTOON', client.user.avatarURL)
     .setThumbnail(client.user.avatarURL)
-    .addField('Version', require('../../../package.json').version, true)
-    .addField('Library', '[Discord.js](http://discord.js.org/)', true)
+    .addField('Version', `v${package.version}`, true)
+    .addField('Library', `[Discord.js](http://discord.js.org/) (v${package.dependencies['discord.js'].split('^')[1]})`, true)
     .addField('Repository', '[Gitlab - lwbot-rewrite](http://gitlab.com/akii0008/lwbot-rewrite)', true)
     .addField('Creator', `<@107599228900999168> (${client.users.get('107599228900999168').tag})`, true)
-    .addField('Special Thanks', '[`An Idiot\'s Guide`](http://anidiots.guide/) - Initial command framework, eval command, reload command, and so many other things. Thank you *so* much.\n[`discordjs.guide`](http://discordjs.guide) - Provided tagging system.\n[`Discord.js Server`](https://discord.gg/bRCvFy9) - You guys answered every question the docs couldn\'t.')
+    .addField('Credits', '[`An Idiot\'s Guide`](http://anidiots.guide/) - Initial command framework, eval command, reload command, and so many other things. Thank you *so* much.\n[`discordjs.guide`](http://discordjs.guide) - Provided tagging system.\n[`Discord.js Server`](https://discord.gg/bRCvFy9) - You guys answered every question the docs couldn\'t.')
   );
 };
 
