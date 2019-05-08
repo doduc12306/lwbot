c1d611fc7887e0a4d1f7c916667e8ae252a1dff2
Akihito Mikazuki <jennasisis@gmail.com>
2019-05-08 18:24:05 -0400

    sqWatchdog: debugMode ? 10 second : 10 minute refresh

--- src/util/sqWatchdog.js
+++ src/util/sqWatchdog.js
@@ -6,6 +6,8 @@ const config = require('../config');
 module.exports = async (client) => {
   logger.log('Watchdog started');
 
+  const time = config.debugMode ? 10000 : 600000; // If debug mode is on, refresh every 10 seconds. Else, 10 minutes.
+
   setInterval(async () => {
     const start = new Date();
     logger.sqLog('Started process...');
@@ -80,7 +82,7 @@ module.exports = async (client) => {
     }));
 
     logger.sqLog(`Ended process! ${new Date() - start}ms`);
-  }, 600000); // Every 10 minutes
+  }, time); // Every 10 minutes
 
 
 };
