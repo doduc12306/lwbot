72365f78a82d3aabe96679fb4da4aa9e13d892e0
jennasisis <jennasisis@gmail.com>
2019-05-12 10:09:10 -0400

    levels: reorganized to new xp db & added levels

--- src/commands/Server/levels.js
+++ src/commands/Server/levels.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
+
 module.exports.run = async (client, message) => {
-  message.guild.xp.findAll({
+  const xpSchema = require('../../dbFunctions/message/xp').functions.xpSchema(message.guild.id);
+
+  xpSchema.findAll({
     order: [
       ['xp', 'DESC'],
       ['id', 'DESC']
@@ -10,18 +13,18 @@ module.exports.run = async (client, message) => {
       .setColor(client.config.colors.green);
     for (const values of data) {
       if (data.indexOf(values) === 0) {
-        if (message.guild.members.get(values.dataValues.user) === undefined) embed.addField(':crown: `[User Left]`', `XP: ${values.dataValues.xp} \`|\` Level: ${undefined}`);
-        else embed.addField(`:crown: ${message.guild.members.get(values.dataValues.user).user.tag}`, `XP: ${values.dataValues.xp} \`|\` Level: ${undefined}`);
+        if (message.guild.members.get(values.dataValues.user) === undefined) embed.addField(':crown: `[User Left]`', `XP: ${values.dataValues.xp} \`|\` Level: ${values.dataValues.level}`);
+        else embed.addField(`:crown: ${message.guild.members.get(values.dataValues.user).user.tag}`, `XP: ${values.dataValues.xp} \`|\` Level: ${values.dataValues.level}`);
       } else if (data.indexOf(values) === 1) {
-        if (message.guild.members.get(values.dataValues.user) === undefined) embed.addField(':second_place: `[User Left]`', `XP: ${values.dataValues.xp} \`|\` Level: ${undefined}`);
-        else embed.addField(`:second_place: ${message.guild.members.get(values.dataValues.user).user.tag}`, `XP: ${values.dataValues.xp} \`|\` Level: ${undefined}`);
+        if (message.guild.members.get(values.dataValues.user) === undefined) embed.addField(':second_place: `[User Left]`', `XP: ${values.dataValues.xp} \`|\` Level: ${values.dataValues.level}`);
+        else embed.addField(`:second_place: ${message.guild.members.get(values.dataValues.user).user.tag}`, `XP: ${values.dataValues.xp} \`|\` Level: ${values.dataValues.level}`);
       } else if (data.indexOf(values) === 2) {
-        if (message.guild.members.get(values.dataValues.user) === undefined) embed.addField(':third_place: `[User Left]`', `XP: ${values.dataValues.xp} \`|\` Level: ${undefined}`);
-        else embed.addField(`:third_place: ${message.guild.members.get(values.dataValues.user).user.tag}`, `XP: ${values.dataValues.xp} \`|\` Level: ${undefined}`);
+        if (message.guild.members.get(values.dataValues.user) === undefined) embed.addField(':third_place: `[User Left]`', `XP: ${values.dataValues.xp} \`|\` Level: ${values.dataValues.level}`);
+        else embed.addField(`:third_place: ${message.guild.members.get(values.dataValues.user).user.tag}`, `XP: ${values.dataValues.xp} \`|\` Level: ${values.dataValues.level}`);
       }
       if (data.indexOf(values) <= 9 && data.indexOf(values) >= 3) {
-        if (message.guild.members.get(values.dataValues.user) === undefined) embed.addField(`#${data.indexOf(values) + 1} \`[User Left]\``, `XP: ${values.dataValues.xp} \`|\` Level: ${undefined}`);
-        else embed.addField(`#${data.indexOf(values) + 1} ${message.guild.members.get(values.dataValues.user).user.tag}`, `XP: ${values.dataValues.xp} \`|\` Level: ${undefined}`);
+        if (message.guild.members.get(values.dataValues.user) === undefined) embed.addField(`#${data.indexOf(values) + 1} \`[User Left]\``, `XP: ${values.dataValues.xp} \`|\` Level: ${values.dataValues.level}`);
+        else embed.addField(`#${data.indexOf(values) + 1} ${message.guild.members.get(values.dataValues.user).user.tag}`, `XP: ${values.dataValues.xp} \`|\` Level: ${values.dataValues.level}`);
       }
     }
     message.send(embed);
