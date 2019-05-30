215aa40c6b458312aadcf34c7a0921888fe67d73
akii0008 <jennasisis@gmail.com>
2019-05-30 17:59:07 -0400

    messageUpdate: removed verbose

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -1,6 +1,5 @@
 module.exports = async (client, oldMessage, newMessage) => {
   if(client.msgCmdHistory[newMessage.id]) {
-    client.verbose('messageUpdate');
     newMessage.edited = true;
     client.emit('message', newMessage);
   }
