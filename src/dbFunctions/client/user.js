6e25ce193ba4c1460c01326b9607ec2fded6f35f
akii0008 <jennasisis@gmail.com>
2019-05-30 17:59:38 -0400

    db()'s/client/user: added changeBalance

--- src/dbFunctions/client/user.js
+++ src/dbFunctions/client/user.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 class User {
-  constructor (userID) {
+  constructor(userID) {
     this.userID = userID;
   }
 
@@ -30,11 +30,42 @@ class User {
   }
 
   get balance() {
-    return this.shortcut().findOrCreate({ where: { key: 'balance' }, defaults: { value: '0' }})
+    return this.shortcut().findOrCreate({ where: { key: 'balance' }, defaults: { value: '0' } })
       .then(res => {
-        res = res[0].dataValues.value;
-        return +res; // + = Number coersion
+        return +res[0].get('value');
       });
+  }
+
+  changeBalance(operation, amount) {
+    if (!operation) throw new Error(`Missing operation to perform on ${this.userID}`);
+    if (!amount) throw new Error(`Missing amount to ${operation}`);
+    switch (operation) {
+      case 'add': {
+        return this.shortcut().findOrCreate({ where: { key: 'balance' }, defaults: { value: '0' } })
+          .then(res => {
+            return this.shortcut().update({ value: (+res[0].get('value') + amount).toString() }, { where: { key: 'balance' } }).then(() => {
+              return +res[0].get('value') + amount;
+            }).catch(e => { throw new Error(e); });
+          }).catch(e => { throw new Error(e); });
+      }
+      case 'subtract': {
+        return this.shortcut().findOrCreate({ where: { key: 'balance' }, defaults: { value: '0' } })
+          .then(res => {
+            return this.shortcut().update({ value: (+res[0].get('value') - amount).toString() }, { where: { key: 'balance' } }).then(() => {
+              return +res[0].get('value') - amount;
+            }).catch(e => { throw new Error(e); });
+          }).catch(e => { throw new Error(e); });
+      }
+      case 'set': {
+        return this.shortcut().findOrCreate({ where: { key: 'balance' }, defaults: { value: '0' } })
+          .then(() => {
+            return this.shortcut().update({ value: amount.toString() }, { where: { key: 'balance' } }).then(() => {
+              return amount;
+            }).catch(e => { throw new Error(e); });
+          }).catch(e => { throw new Error(e); });
+      }
+      default: throw new Error('Operation must be one of "add", "subtract", or "set".');
+    }
   }
 
   get moto() {
