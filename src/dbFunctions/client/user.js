bc8d5ee1cdf728a1f4aac9f2ea2c2d4dd166883a
akii0008 <jennasisis@gmail.com>
2019-06-09 18:49:48 -0400

    db()'s/client/user: added some functions

--- src/dbFunctions/client/user.js
+++ src/dbFunctions/client/user.js
@@ -37,8 +37,7 @@ class User {
   }
 
   changeBalance(operation, amount) {
-    if (!operation) throw new Error(`Missing operation to perform on ${this.userID}`);
-    if (!amount) throw new Error(`Missing amount to ${operation}`);
+    if (!amount) throw new Error('Missing amount parameter');
     switch (operation) {
       case 'add': {
         return this.shortcut().findOrCreate({ where: { key: 'balance' }, defaults: { value: '0' } })
@@ -51,6 +50,9 @@ class User {
       case 'subtract': {
         return this.shortcut().findOrCreate({ where: { key: 'balance' }, defaults: { value: '0' } })
           .then(res => {
+            if (+res[0].get('value') <= 0) return false;
+            if (+res[0].get('value') - amount <= 0) return false;
+
             return this.shortcut().update({ value: (+res[0].get('value') - amount).toString() }, { where: { key: 'balance' } }).then(() => {
               return +res[0].get('value') - amount;
             }).catch(e => { throw new Error(e); });
@@ -68,12 +70,21 @@ class User {
     }
   }
 
-  get moto() {
-    return this.shortcut().findOrCreate({ where: { key: 'moto' }, defaults: { value: 'Great day to set up the moto!' } })
+  get mood() {
+    return this.shortcut().findOrCreate({ where: { key: 'mood' }, defaults: { value: 'Great day to set your mood!' } })
       .then(res => {
         res = res[0].dataValues.value;
         return res;
       });
+  }
+
+  changeMood(newMood) {
+    if (!newMood) throw new Error('Missing mood parameter');
+    return this.shortcut().findOrCreate({ where: { key: 'mood' }, defaults: { value: 'Great day to set your mood!' } })
+      .then(() => {
+        this.shortcut().update({ value: newMood }, { where: { key: 'mood' } }).catch(e => { throw new Error(e); });
+        return newMood;
+      }).catch(e => { throw new Error(e); });
   }
 
   get badges() {
@@ -81,18 +92,80 @@ class User {
       .then(res => {
         res = res[0].dataValues.value;
         return res;
-      });
+      }).catch(e => { throw new Error(e); });
+  }
+
+  changeBadges(operation, badgeName) {
+    switch (operation) {
+      case 'add': {
+        return this.shortcut().findOrCreate({ where: { key: 'badges' }, defaults: { value: '' } })
+          .then(res => {
+            const badgeArray = res[0].get('value').split(' ');
+            if (badgeArray.includes(badgeName)) return false; // If the array has it already, dont go further.
+
+            this.shortcut().update({ value: `${res[0].get('value')} ${badgeName}` }).catch(e => { throw new Error(e); });
+            badgeArray.push(badgeName);
+            return badgeArray;
+          }).catch(e => { throw new Error(e); });
+      }
+      case 'remove': {
+        return this.shortcut().findOrCreate({ where: { key: 'badges' }, defaults: { value: '' } })
+          .then(res => {
+            const badgeArray = res[0].get('value').split(' ');
+            if (!badgeArray.includes(badgeName)) return false; // If the array does not have the badge, dont go further.
+
+            const updated = badgeArray.splice(badgeArray.indexOf(badgeName), 1).join(' '); // Find the index of the badge, then remove (splice) it from that index
+            this.shortcut().update({ value: updated }, { where: { key: 'badges' } }).catch(e => { throw new Error(e); });
+            return updated;
+          }).catch(e => { throw new Error(e); });
+      }
+      default: throw new Error('Operation must be one of "add" or "remove"');
+    }
   }
 
   get reputation() {
     return this.shortcut().findOrCreate({ where: { key: 'reputation' }, defaults: { value: '0' } })
       .then(res => {
         res = res[0].dataValues.value;
-        return +res; // + = Number coersion
+        return +res;
       });
   }
 
-  // TODO: Add set methods as well.
+  changeReputation(operation, amount) {
+    if (!operation) throw new Error('Missing operation parameter');
+    if (!amount) throw new Error('Missing amount parameter');
+    switch (operation) {
+      case 'add': {
+        return this.shortcut().findOrCreate({ where: { key: 'reputation' }, defaults: { value: '0' } })
+          .then(res => {
+            return this.shortcut().update({ value: (+res[0].get('value') + amount).toString() }, { where: { key: 'reputation' } })
+              .then(() => { return +res[0].get('value') + amount; })
+              .catch(e => { throw new Error(e); });
+          }).catch(e => { throw new Error(e); });
+      }
+      case 'subtract': {
+        return this.shortcut().findOrCreate({ where: { key: 'reputation' }, defaults: { value: '0' } })
+          .then(res => {
+            if (+res[0].get('value') <= 0) return false;
+            if (+res[0].get('value') - amount < 0) return false;
+
+            return this.shortcut().update({ value: (+res[0].get('value') - amount).toString() }, { where: { key: 'reputation' } })
+              .then(() => { return +res[0].get('value') - amount; })
+              .catch(e => { throw new Error(e); });
+          }).catch(e => { throw new Error(e); });
+      }
+      case 'set': {
+        return this.shortcut().findOrCreate({ where: { key: 'reputation' }, defaults: { value: '0' } })
+          .then(() => {
+            return this.shortcut().update({ value: amount.toString() }, { where: { key: 'reputation' } }).then(() => {
+              return amount;
+            }).catch(e => { throw new Error(e); });
+          }).catch(e => { throw new Error(e); });
+      }
+      default: throw new Error('Operation must be one of "add", "subtract", or "set".');
+    }
+  }
+
 }
 
 module.exports = User;
