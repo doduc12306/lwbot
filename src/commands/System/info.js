52b65f7f0f5b0c42b44fe251be0dc11675064a92
Akihito Mikazuki <jennasisis@gmail.com>
2019-07-03 19:36:30 -0400

    info: fixed discord.js version number

--- src/commands/System/info.js
+++ src/commands/System/info.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 const package = require('../../../package.json');
+
 module.exports.run = (client, message) => {
   message.send(new Discord.RichEmbed()
     .setColor(message.guild.accentColor)
     .setAuthor('LINE WEBTOON', client.user.avatarURL)
     .setThumbnail(client.user.avatarURL)
     .addField('Version', `v${package.version}`, true)
-    .addField('Library', `[Discord.js](http://discord.js.org/) (v${package.dependencies['discord.js'].split('^')[1]})`, true)
+    .addField('Library', `[Discord.js](http://discord.js.org/) (v${package.dependencies['discord.js']})`, true)
     .addField('Repository', '[Gitlab - lwbot-rewrite](http://gitlab.com/akii0008/lwbot-rewrite)', true)
     .addField('Creator', `<@107599228900999168> (${client.users.get('107599228900999168').tag})`, true)
     .addField('Credits', '[`An Idiot\'s Guide`](http://anidiots.guide/) - Initial command framework, eval command, reload command, and so many other things. Thank you *so* much.\n[`discordjs.guide`](http://discordjs.guide) - Provided tagging system.\n[`Discord.js Server`](https://discord.gg/bRCvFy9) - You guys answered every question the docs couldn\'t.')
