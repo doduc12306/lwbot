841f4c263261720b5a92dfcb5196895aa7b1ff4a
Akihito Mikazuki <jennasisis@gmail.com>
2019-07-30 14:07:36 -0400

    ping: removed embed was actually slowing down the command and thus skewing the latency

--- src/commands/System/ping.js
+++ src/commands/System/ping.js
@@ -1,27 +1,10 @@
-const Discord = require('discord.js');
-
-module.exports.run = (client, message) => {
-  const pings = client.pings.filter(g => g !== undefined).join(', ');
-
-  message.send(new Discord.RichEmbed()
-    .addField('🏓 Ping', 'Pinging... (Pinging...)')
-    .addField('↔ Latency', 'Pinging...')
-    .setColor(client.config.colors.yellow)
-  ).then(thismessage => {
-    thismessage.edit(new Discord.RichEmbed()
-      .addField('🏓 Ping', `${Math.round(client.ping)}ms (${pings})`)
-      .addField('↔ Latency', `${thismessage.createdAt-message.createdAt}ms`)
-      .setColor(message.guild.accentColor)
-    );
-  });
-};
+module.exports.run = (client, message) => message.send(`:ping_pong: \`${Math.round(client.ping)}ms\``).then(msg => msg.edit(`${msg.content} • **Latency:** \`${msg.createdAt - message.createdAt}ms\``));
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: ['pong', 'p', 'pnig', '🏓'],
-  permLevel: 'User',
-  requiresEmbed: true
+  permLevel: 'User'
 };
 
 exports.help = {
