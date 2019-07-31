8a6c9716090f4532761821475100cac88baf781b
jennasisis <jennasisis@gmail.com>
2019-07-30 21:12:23 -0400

    rep: fixed

--- src/commands/Economy/rep.js
+++ src/commands/Economy/rep.js
@@ -1,13 +1,13 @@
 const User = require('../../dbFunctions/client/user');
 module.exports = {
-  run: (client, message) => {
+  run: async (client, message) => {
     const user = message.mentions.users.first();
 
-    if(!user) message.send('❌ `|` ⬆ **You didn\'t mention a user to add reputation to.**');
-    if(user.id === message.author.id) message.send('❌ `|` ⬆ **You can\'t add reputation to yourself.**');
+    if(!user) return message.send('❌ `|` ⬆ **You didn\'t mention a user to add reputation to.**');
+    if(user.id === message.author.id) return message.send('❌ `|` ⬆ **You can\'t add reputation to yourself.**');
 
     const addRep = new User(user.id).changeReputation('add', 1);
-    message.send(`⬆ **+1 reputation to** ${user}**!** Total: \`${addRep}\``);
+    message.send(`⬆ **+1 reputation to** ${user}**!** Total: \`${await addRep}\``);
   },
 
   conf: {
