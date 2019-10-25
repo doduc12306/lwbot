755618a4a8d9846ae1a53ffea5be98b9953bcdfa
akii0008 <jennasisis@gmail.com>
2019-10-24 21:26:48 -0400

    startup: lots of changes startup is now async added section markers for readability moved promisified fs functions to top of file added log compression for every log folder that isnt the current day's, then that function set on a 24 hour interval command counting

--- src/startup.js
+++ src/startup.js
@@ -22,16 +22,17 @@ module.exports.client = new Discord.Client({
   ws: { large_threshold: 1000 }
 });
 
-module.exports.startup = () => {
-  if(global.failover) console.warn('FAILOVER INITIATED!');
+module.exports.startup = async () => {
+  if (global.failover) console.warn('FAILOVER INITIATED!');
 
+  /* SECTION: CLI ARGUMENTS PARSING */
   const options = commandLineArgs([
     // Modes
     { name: 'debug', alias: 'd', type: Boolean },
     { name: 'verbose', alias: 'v', type: Boolean },
     { name: 'sqLog', alias: 's', type: Boolean },
     { name: 'ciMode', type: Boolean },
-  
+
     // Tokens
     { name: 'token', type: String },
     { name: 'debugToken', type: String },
@@ -44,7 +45,7 @@ module.exports.startup = () => {
 
   const client = this.client;
   client.config = require('./config.js');
-  
+
   // This is down here because client isn't defined by the time cli args are.
   if (options.debug) client.config.debugMode = true;
   if (options.verbose) client.config.verboseMode = true;
@@ -53,58 +54,78 @@ module.exports.startup = () => {
   if (options.debugToken) process.env.DEBUG_TOKEN = options.debugToken;
   if (options.googleAPIKey) process.env.GOOGLE_API_KEY = options.googleAPIKey;
   if (options.noFileLog) client.config.noFileLog = true;
-  
-  const readdir = promisify(fs.readdir);
-  const writeFile = promisify(fs.writeFile);
-  const mkdir = promisify(fs.mkdir);
-  
-  client.commands = new Enmap();
-  client.aliases = new Enmap();
-  client.folder = new Enmap();
-  client.cooldowns = new Enmap();
-  
-  client.before = new Date();
-  
-  const currentDayLog = new Date().toDateString().replace(/ +/g, '-'); // Gets current date, and formats it
-  require('os').platform() === 'win32' // Makes ../logs/ if it not already exist
-    ? mkdir(`logs/${currentDayLog}/`, { recursive: true }, e => { if (e) throw new Error(e); }) // Windows mkdir
-    : mkdir('logs/', { recursive: true }, e => { // Not windows mkdir
-      if (e && e.code === 'EEXIST') return;
-      if (e) throw new Error(e);
-    });
-  
-  client.logger = require('./util/Logger');
-  
+  /* END SECTION */
+
+  /* SECTION: CLIENT DEFINITIONS */
+  client.commands = new Enmap(); // Where all the commands are kept
+  client.aliases = new Enmap(); // Where all the aliases to each command are kept
+  client.folder = new Enmap(); // Where all command categories are kept
+  client.cooldowns = new Enmap(); // Where all user-command cooldowns are kept
+  client.logger = require('./util/Logger'); // Logger class interface for the rest of the client
+
+  client.logger.log('STARTING BOT...');
+
   require('./dbFunctions/client/misc.js')(client);
   require('./dbFunctions/client/protos.js')(client);
-  
-  if (options.ciMode) {
-    client.config.ciMode = true;
-    client.config.debugMode = true;
-    client.config.verboseMode = true;
-  
-    client.logger.debug('CI MODE ENABLED - RUNNING TESTS AND EXITING');
-    return require('./util/ci')(client);
+
+  client.before = new Date(); // Boot timestamp - Initial
+  /* END SECTION */
+
+  const curDay = moment().format('YYYY-MM-DD');
+  /* SECTION: LOG DIRECTORY CREATION */
+  mkdir(`logs/${curDay}`).then(() => client.logger.log(`Created log directory for today: ${curDay}`)).catch(e => {
+    if (e.code === 'EEXIST') return;
+    else client.logger.log(e);
+  });
+  /* END SECTION */
+  /* SECTION: LOG COMPRESSION */
+  await compressLogs();
+  async function compressLogs() {
+    const logsDir = await readdir('./logs');
+
+    for (const log of logsDir) {
+      const logIsDir = await stat(`./logs/${log}`).then(g => g.isDirectory());
+      if (log !== curDay && logIsDir) { // Start compressiom
+        client.logger.log(`Found uncompressed log, ${log}. Compressing now...`);
+        compressing.tgz.compressDir(`./logs/${log}`, `./logs/${log}.tar.gz`)
+          .then(async () => { await remove(`./logs/${log}`).then(() => client.logger.log(`Compression of ./logs/${log} complete`)); })
+          .catch(e => client.logger.error(`There was an error compressing ./logs/${log}:\n\t${e}`));
+      }
+    }
   }
-  
-  client.logger.log('STARTING BOT...');
-  
-  // Here we load commands into memory, as a collection, so they're accessible
-  // here and everywhere else.
+  setInterval(compressLogs, 8.64e+7); // Run the compressLogs function every 24 hours.
+  /* END SECTION */
+
+  if (options.ciMode) { // If CI mode was enabled,
+    client.config.ciMode = true; // ... enable ci mode in the config
+    client.config.debugMode = true; // ... enable debug mode (debug bot)
+    client.config.verboseMode = true; // ... enable verbose logging
+
+    client.logger.debug('CI MODE ENABLED - RUNNING TESTS AND EXITING');
+    return require('./util/ci')(client); // ... and run the CI test suite
+  }
+
+  /* SECTION: COMMAND LOADING */
+  // Here we load commands into memory, as a collection, so they're accessible here and everywhere else
   const cmdFiles = walk.walk('./commands/', { followLinks: false, filters: ['Temp', '_Temp'] });
   client.logger.log('Loading commands...');
+  let cmdCount = 0;
   cmdFiles.on('file', (root, fileStats, next) => {
     const cmdPath = require('os').platform().includes('win')
       ? root.substring(root.indexOf('commands\\') + 13) // Windows path finding
       : join(__dirname, root).substring(join(__dirname, root).indexOf('commands/') + 9); // Linux path finding
-  
+
     const response = client.loadCommand(cmdPath, fileStats.name);
-    if (response) client.logger.error(response);
-    next();
+    if (response) client.logger.error(response); // If there was an error loading the command, output it
+
+    cmdCount++;
+    next(); // Ready for the next command...
   });
-  
+
   cmdFiles.on('end', async () => {
-    await client.logger.log('All commands finished loading!');
+    await client.logger.log(`${cmdCount} commands finished loading!`);
+    /* END SECTION */
+    /* SECTION: EVENT LOADING */
     // Then we load events, which will include our message and ready event.
     const evtFiles = await readdir('./events/');
     client.logger.log(`Loading a total of ${evtFiles.length} events.`);
@@ -115,23 +136,27 @@ module.exports.startup = () => {
       await client.logger.log(`Loaded ${file} event`);
     });
     await client.logger.log('All events finished loading!');
-  
+    /* END SECTION */
+
+    /* SECTION: PERMISSION LEVEL GENERATION */
     // Generate a cache of client permissions for pretty perms
     client.levelCache = {};
     for (let i = 0; i < client.config.permLevels.length; i++) {
       const thisLevel = client.config.permLevels[i];
       client.levelCache[thisLevel.name] = thisLevel.level;
     }
-  
+    /* END SECTION */
+
+    // Login to Discord websocket. Triggers 'ready' event on login
     client.login(client.config.debugMode ? process.env.DEBUG_TOKEN : process.env.TOKEN);
   });
-  
+
   // Events that don't require their own files since they're so minor
   client.on('disconnect', () => client.logger.log('Client disconnected!', 'disconnect'));
   client.on('reconnecting', () => client.logger.log('Reconnecting...', 'reconnecting'));
   client.on('resume', replayed => client.logger.log(`Client resumed! Replayed ${replayed} events`, 'resume'));
   client.on('warn', info => client.logger.warn(`Warning: "${info}"`));
-  
+
   // Error handling
   process.on('uncaughtException', async (err) => {
     if (err.stack.trim().includes('at WebSocketConnection.onError')) {
@@ -157,10 +182,10 @@ module.exports.startup = () => {
       });
       process.exit(1);
     }
-  
+
     process.exit(1); // Even if the previous don't run for whatever reason, this will.
   });
-  
+
   process.on('unhandledRejection', err => {
     if (err.message === 'Please install sqlite3 package manually') {
       client.logger.error('sqlite3 package needs to be reinstalled.');
@@ -176,5 +201,5 @@ module.exports.startup = () => {
     }
     client.logger.error(`Unhandled rejection: ${err.stack}`);
   });
-  
+
 };
