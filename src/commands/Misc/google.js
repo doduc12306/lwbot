c7db37f90dbe30961c3c39947d2003171198b680
akii0008 <jennasisis@gmail.com>
2019-12-30 14:07:14 -0500

    google: working google command!

---
+++ src/commands/Misc/google.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+const google = require('google-it');
+module.exports.run = async (client, message, args) => {
+  const query = args.join(' ');
+
+  if (!query) return message.send(':x: `|` :mag_right: **You didn\'t search for anything!**');
+
+  const msg = await message.send('<a:loading:536942274643361794> `|` :mag_right: **Loading...**');
+
+  const options = { 'disableConsole': true };
+  google({ options, 'query': query })
+    .then(res => {
+      const result = res[0];
+      msg.edit(`:mag_right: \`${result.title}\` - ${result.link}`);
+    }).catch(e => {
+      msg.edit(`:x: \`|\` :mag_right: **An error occurred.**\n\`\`\`${e}\`\`\``);
+      client.logger.error(e.stack);
+    });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 'User'
+};
+
+exports.help = {
+  name: 'google',
+  description: 'Google something',
+  usage: 'google <query>',
+  category: 'Misc'
+};
