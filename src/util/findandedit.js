5e62a001d453cae81ff3c336f22166684903dadd
akii0008 <jennasisis@gmail.com>
2019-12-30 16:14:25 -0500

    util/findandedit.js ytdl-core-discord

---
+++ src/util/findandedit.js
@@ -0,0 +1,49 @@
+const fs = require('fs-extra');
+const package = require('../../package.json');
+const ytdlcorediscordVersion = package.dependencies['ytdl-core-discord'];
+
+console.log('ytdl-core-discord version:', ytdlcorediscordVersion);
+
+if(ytdlcorediscordVersion !== '^1.0.3') {
+  console.error('Version mismatch!');
+  console.error('  Expected ytdl-core-discord version: ^1.0.3');
+  console.error('  Actual ytdl-core-discord version:  ', ytdlcorediscordVersion);
+  process.exit(1);
+}
+
+fs.readFile('../../node_modules/ytdl-core/lib/index.js', (err, data) => {
+  if(err) throw err;
+  data = data.toString();
+  data = data.split('\n');
+  console.log('Original data:', data[6]);
+
+  data[6] = 'const parseTime   = require(\'m3u8stream/dist/parse-time\');';
+
+  console.log('Edited data:', data[6]);
+
+  data = data.join('\n');
+
+  fs.writeFile('../../node_modules/ytdl-core/lib/index.js', data, err => {
+    if(err) throw err;
+    console.log('Edited node_modules/ytdl-core/lib/index.js');
+  });
+});
+
+fs.readFile('../../node_modules/ytdl-core/lib/info-extras.js', (err, data) => {
+  if(err) throw err;
+  data = data.toString();
+  data = data.split('\n');
+
+  console.log('Original data:', data[4]);
+
+  data[4] = 'const parseTime   = require(\'m3u8stream/dist/parse-time\');';
+
+  console.log('Edited data:', data[4]);
+
+  data = data.join('\n');
+
+  fs.writeFile('../../node_modules/ytdl-core/lib/info-extras.js', data, err => {
+    if(err) throw err;
+    console.log('Edited node_modules/ytdl-core/lib/info-extras.js');
+  });
+});
