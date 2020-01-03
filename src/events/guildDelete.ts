8969ff49de33aeb71a6df1c0f7fd74fb1c60dfc9
akii0008 <jennasisis@gmail.com>
2020-01-03 09:06:26 -0500

    guildDelete.js -> guildDelete.ts because of the optional chaining new in ts3.7

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -1,13 +1,14 @@
 const { unlink } = require('fs-extra');
+
 module.exports = (client, guild) => {
   client.logger.log(`Left guild ${guild.name} (${guild.id})`);
 
   unlink(`databases/servers/${guild.id}.sqlite`, err => {
-    if(err && err.code === 'ENOENT') return client.logger.warn(`guildDelete: Database ${guild.id} did not exist to delete. Did sqWatchdog get to it before I did?`);
+    if(err?.code === 'ENOENT') return client.logger.warn(`guildDelete: Database ${guild.id} did not exist to delete. Did sqWatchdog get to it before I did?`);
     if(err) throw err;
 
     client.logger.log(`Deleted guild database ${guild.id}.sqlite`);
   });
 
-  delete client.settings.get(guild.id);
+  client.settings.delete(guild.id);
 };
