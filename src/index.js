b42ea1e36ebabbf80b88a051e7527cbef7bd536c
akii0008 <samir.js.zip@gmail.com>
2020-03-25 12:33:56 -0400

    index: .env checking

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const logger = require('./util/Logger');
+const fs = require('fs-extra');
 if (process.version.slice(1).split('.')[0] < 12) {
   logger.error('Invalid Node.js version. Please choose a version greater than 11. (Preferrably v12)');
   process.exit(1);
 }
+
+fs.access('../.env', fs.constants.F_OK, err => { if(err) { logger.error('.env file does not exist.'); process.exit(1); } });
 
 require('./startup').startup();
 
