28259ad703a7263a83c3a758e15c77bec95c67de
akii0008 <jennasisis@gmail.com>
2020-03-27 18:02:19 -0400

    addemote: optimizations

--- src/commands/Server/addemote.js
+++ src/commands/Server/addemote.js
@@ -1,22 +1,31 @@
 module.exports.run = (client, message, args) => {
-  if (message.member.permissions.has()('MANAGE_EMOJIS')) {
-    const emojiName = args[0];
-    const emojiURL = args[1];
+  if (!message.member.permissions.has('MANAGE_EMOJIS')) return message.send('❌ **You are missing a permission:** `Manage Emojis`');
+  if(!message.guild.me.permissions.has('MANAGE_EMOJIS')) return message.send(':x: **I am missing a permission:** `Manage Emojis`');
 
-    if (!emojiName) return message.send('❌ You forgot the emoji name!');
-    if (!emojiURL) return message.send('❌ You forgot the emoji url!');
+  const emojiName = args[0];
+  let emojiURL = args[1];
 
-    message.guild.emojis.create(emojiURL, emojiName, null, `${message.author.tag} created emoji ${emojiName}`)
-      .then(emote => message.send(`✅ Emote **\`${emote.name}\`** ${emote} created!`))
-      .catch(err => message.send(`❌ Something went wrong:\n${err}`));
+  if (!emojiName) return message.send('❌ You forgot the emoji name!');
+  if (!args[1] && message.attachments.size === 0) return message.send('❌ You forgot the emoji url!');
+  if (!args[1] && message.attachments.size > 0) emojiURL = message.attachments.first().url;
 
-  } else message.send('❌ Missing Permission: `Manage Emojis`');
+  // attachment, name
+  message.guild.emojis.create(emojiURL, emojiName, { reason: `${message.author.tag} created emoji ${emojiName}` })
+    .then(emote => message.send(`✅ **Emote \`${emote.name}\` ${emote} created!**`))
+    .catch(err => {
+      if (err.message.includes('File cannot be larger than 256.0 kb')) return message.send(':x: **Your image file size is too large!** Must not be larger than 256kb.');
+      else if (err.message.includes('Maximum number of emojis reached (50)')) return message.send(':x: **Your server already has the maximum amount of emojis of this type!**');
+      else {
+        message.send(`❌ **Something went wrong:**\n${err}`);
+        client.logger.error(err);
+      }
+    });
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ['ae', 'addemoji', 'createemote', 'emojis.create'],
+  aliases: ['ae', 'addemoji', 'createemote', 'createemoji'],
   permLevel: 'Moderator'
 };
 
@@ -24,5 +33,5 @@ exports.help = {
   name: 'addemote',
   description: 'Adds an emote to the server',
   category: 'Server',
-  usage: 'addemote <name> <image url>'
+  usage: 'addemote <name> <image url / attachment>'
 };
