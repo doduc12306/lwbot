5f5a2af9ec2f828fc41002b7bde8687804609a65
akii0008 <jennasisis@gmail.com>
2020-03-27 21:39:53 -0400

    created mood command allows you to change your mood displayed on your userinfo profile

---
+++ src/commands/User/mood.js
@@ -0,0 +1,25 @@
+const userMethods = require('../../dbFunctions/client/user');
+
+module.exports.run = async (client, message, args) => {
+  const mood = args.join(' ');
+  if(!mood) return message.send('❌ `|` ✏️ **You didn\'t give a new mood to set!');
+
+  const User = new userMethods(message.author.id);
+  User.changeMood(mood);
+
+  message.send(`✅ \`|\` ✏️ **Changed mood to** \`${mood}\``);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  permLevel: 'User',
+  aliases: ['setmood']
+};
+
+exports.help = {
+  name: 'mood',
+  description: 'Set your mood',
+  usage: 'mood <new mood>',
+  category: 'User'
+};
