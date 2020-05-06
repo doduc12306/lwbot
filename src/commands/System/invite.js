2d7e909555997299e63b0788c2d8bb825b0d29c6
akii0008 <jennasisis@gmail.com>
2020-05-05 23:14:43 -0400

    invite: dont generate admin invite, use actual perms

--- src/commands/System/invite.js
+++ src/commands/System/invite.js
@@ -3,7 +3,32 @@ module.exports.run = async (client, message) => {
   message.send(new MessageEmbed()
     .setColor(client.accentColor)
     .setTitle('Invite me to another server!')
-    .setDescription(await client.generateInvite(['ADMINISTRATOR']))
+    .setDescription(await client.generateInvite([
+      'CREATE_INSTANT_INVITE', 
+      'KICK_MEMBERS', 
+      'BAN_MEMBERS', 
+      'MANAGE_CHANNELS', 
+      'MANAGE_GUILD', 
+      'ADD_REACTIONS', 
+      'VIEW_AUDIT_LOG', 
+      'READ_MESSAGES', 
+      'SEND_MESSAGES', 
+      'MANAGE_MESSAGES', 
+      'EMBED_LINKS', 
+      'ATTACH_FILES', 
+      'READ_MESSAGE_HISTORY', 
+      'USE_EXTERNAL_EMOJIS',
+      'CONNECT', 
+      'SPEAK', 
+      'MUTE_MEMBERS', 
+      'DEAFEN_MEMBERS', 
+      'USE_VAD', 
+      'CHANGE_NICKNAME', 
+      'MANAGE_ROLES', 
+      'MANAGE_WEBHOOKS', 
+      'MANAGE_EMOJIS', 
+      'STREAM'
+    ]))
   );
 };
 
